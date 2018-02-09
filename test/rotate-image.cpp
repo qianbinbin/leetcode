ff2dfe817393760fd148d80d1fe6797d502fc4a7
Binbin Qian <qianbinbin@hotmail.com>
2018-02-09 23:58:48 +0800

    48. Rotate Image

---
+++ test/rotate-image.cpp
@@ -0,0 +1,29 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+#include <common.h>
+#include <rotate-image.h>
+}
+
+TEST(leetcode_48, normal) {
+    int **matrix = (int **) malloc(3 * sizeof(int *));
+    for (int i = 0; i < 3; ++i) {
+        int *nums = (int *) malloc(3 * sizeof(int));
+        nums[0] = 3 * i + 1;
+        nums[1] = 3 * i + 2;
+        nums[2] = 3 * i + 3;
+        matrix[i] = nums;
+    }
+    for (int i = 0; i < 3; ++i) {
+        print_array(matrix[i], 3);
+    }
+    printf("\n");
+
+    int n = 3;
+    rotate_48(matrix, n, &n);
+    for (int i = 0; i < 3; ++i) {
+        print_array(matrix[i], 3);
+        free(matrix[i]);
+    }
+    free(matrix);
+}
