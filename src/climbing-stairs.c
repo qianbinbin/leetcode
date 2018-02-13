b75bc38d5c836ac4ef683f6f8d5a1e13928bff00
Binbin Qian <qianbinbin@hotmail.com>
2018-02-14 00:59:49 +0800

    70. Climbing Stairs

---
+++ src/climbing-stairs.c
@@ -0,0 +1,14 @@
+#include <climbing-stairs.h>
+
+int climbStairs_70(int n) {
+    if (n < 1) return 0;
+
+    int pre = 0, cur = 1;
+    int tmp;
+    for (int i = 1; i <= n; ++i) {
+        tmp = pre + cur;
+        pre = cur;
+        cur = tmp;
+    }
+    return cur;
+}
