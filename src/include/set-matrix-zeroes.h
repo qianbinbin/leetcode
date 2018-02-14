e4effff21021e8f0fd7dc7e5172a6518285529e4
Binbin Qian <qianbinbin@hotmail.com>
2018-02-14 23:35:30 +0800

    73. Set Matrix Zeroes

---
+++ src/include/set-matrix-zeroes.h
@@ -0,0 +1,15 @@
+#ifndef LEETCODE_SET_MATRIX_ZEROES_H
+#define LEETCODE_SET_MATRIX_ZEROES_H
+
+/**
+ * Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in place.
+ *
+ * Follow up:
+ * Did you use extra space?
+ * A straight forward solution using O(mn) space is probably a bad idea.
+ * A simple improvement uses O(m + n) space, but still not the best solution.
+ * Could you devise a constant space solution?
+ */
+void setZeroes_73(int **matrix, int matrixRowSize, int matrixColSize);
+
+#endif //LEETCODE_SET_MATRIX_ZEROES_H
