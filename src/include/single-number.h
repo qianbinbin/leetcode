40cb6b46c8107721e7f01ca24db74a7f3a190a0e
Binbin Qian <qianbinbin@hotmail.com>
2018-02-26 15:29:12 +0800

    136. Single Number

---
+++ src/include/single-number.h
@@ -0,0 +1,12 @@
+#ifndef LEETCODE_SINGLE_NUMBER_H
+#define LEETCODE_SINGLE_NUMBER_H
+
+/**
+ * Given an array of integers, every element appears twice except for one. Find that single one.
+ *
+ * Note:
+ * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ */
+int singleNumber_136(int *nums, int numsSize);
+
+#endif //LEETCODE_SINGLE_NUMBER_H
