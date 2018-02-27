b7919bae7670637558f26a1e150a540a11492d04
Binbin Qian <qianbinbin@hotmail.com>
2018-02-27 21:12:53 +0800

    ListNode

--- src/common.c
+++ src/common.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <common.h>
+#include <stdlib.h>
 
 void print_array(int *nums, int numsSize) {
     if (nums == NULL) return;
@@ -9,4 +10,43 @@ void print_array(int *nums, int numsSize) {
         else printf("%d", nums[numsSize - 1]);
     }
     printf("}\n");
+}
+
+void print_list(struct ListNode *p) {
+    while (p != NULL) {
+        if (p->next != NULL) printf("%d->", p->val);
+        else printf("%d", p->val);
+        p = p->next;
+    }
+    printf("\n");
+}
+
+struct ListNode *append_list(struct ListNode *p, int val) {
+    struct ListNode *tmp = (struct ListNode *) malloc(sizeof(struct ListNode));
+    tmp->val = val;
+    tmp->next = NULL;
+    p->next = tmp;
+    return tmp;
+}
+
+struct ListNode *array_to_list(int *nums, int numsSize) {
+    if (nums == NULL || numsSize < 1) return NULL;
+
+    struct ListNode *list = (struct ListNode *) malloc(sizeof(struct ListNode));
+    struct ListNode *p = list;
+    for (int i = 0; i < numsSize; ++i) {
+        p = append_list(p, nums[i]);
+    }
+    p = list->next;
+    free(list);
+    return p;
+}
+
+void free_list(struct ListNode *p) {
+    struct ListNode *pre;
+    while (p != NULL) {
+        pre = p;
+        p = pre->next;
+        free(pre);
+    }
 }
--- src/include/common.h
+++ src/include/common.h
@@ -3,4 +3,17 @@
 
 void print_array(int *nums, int numsSize);
 
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+void print_list(struct ListNode *p);
+
+struct ListNode *append_list(struct ListNode *p, int val);
+
+struct ListNode *array_to_list(int *nums, int numsSize);
+
+void free_list(struct ListNode *p);
+
 #endif //LEETCODE_COMMON_H
