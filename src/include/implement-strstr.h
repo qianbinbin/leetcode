a154dfe569bb332fd4f1695efe43e46cfdc63667
Binbin Qian <qianbinbin@hotmail.com>
2018-03-03 22:27:58 +0800

    28. Implement strStr() - approach 1: brute force

---
+++ src/include/implement-strstr.h
@@ -0,0 +1,25 @@
+#ifndef LEETCODE_IMPLEMENT_STRSTR_H
+#define LEETCODE_IMPLEMENT_STRSTR_H
+
+/**
+ * Implement strStr() (http://www.cplusplus.com/reference/cstring/strstr/).
+ *
+ * Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+ *
+ * Example 1:
+ *
+ * Input: haystack = "hello", needle = "ll"
+ * Output: 2
+ *
+ * Example 2:
+ *
+ * Input: haystack = "aaaaa", needle = "bba"
+ * Output: -1
+ */
+
+/**
+ * Brute force
+ */
+int strStr_28_1(char *haystack, char *needle);
+
+#endif //LEETCODE_IMPLEMENT_STRSTR_H
