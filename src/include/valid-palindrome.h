7aaed2b9fc3ca7b972604f79fe08859b7c0e6b49
Binbin Qian <qianbinbin@hotmail.com>
2018-03-03 21:45:48 +0800

    125. Valid Palindrome

---
+++ src/include/valid-palindrome.h
@@ -0,0 +1,20 @@
+#ifndef LEETCODE_VALID_PALINDROME_H
+#define LEETCODE_VALID_PALINDROME_H
+
+#include <stdbool.h>
+
+/**
+ * Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+ *
+ * For example,
+ * "A man, a plan, a canal: Panama" is a palindrome.
+ * "race a car" is not a palindrome.
+ *
+ * Note:
+ * Have you consider that the string might be empty? This is a good question to ask during an interview.
+ *
+ * For the purpose of this problem, we define empty string as valid palindrome.
+ */
+bool isPalindrome_125(char *s);
+
+#endif //LEETCODE_VALID_PALINDROME_H
