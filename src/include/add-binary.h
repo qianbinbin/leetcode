1580d76db940a770e823629ec146974497ddc0be
Binbin Qian <qianbinbin@hotmail.com>
2018-03-04 21:31:38 +0800

    67. Add Binary

---
+++ src/include/add-binary.h
@@ -0,0 +1,14 @@
+#ifndef LEETCODE_ADD_BINARY_H
+#define LEETCODE_ADD_BINARY_H
+
+/**
+ * Given two binary strings, return their sum (also a binary string).
+ *
+ * For example,
+ * a = "11"
+ * b = "1"
+ * Return "100".
+ */
+char *addBinary_67(char *a, char *b);
+
+#endif //LEETCODE_ADD_BINARY_H
