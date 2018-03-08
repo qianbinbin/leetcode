7b4f0d8cd2e2784180111fc8da2686544283cde9
Binbin Qian <qianbinbin@hotmail.com>
2018-03-08 23:29:02 +0800

    14. Longest Common Prefix

---
+++ src/include/longest-common-prefix.h
@@ -0,0 +1,9 @@
+#ifndef LEETCODE_LONGEST_COMMON_PREFIX_H
+#define LEETCODE_LONGEST_COMMON_PREFIX_H
+
+/**
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ */
+char *longestCommonPrefix_14(char **strs, int strsSize);
+
+#endif //LEETCODE_LONGEST_COMMON_PREFIX_H
---
+++ test/longest-common-prefix.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+#include <longest-common-prefix.h>
+}
+
+TEST(leetcode_14, empty) {
+    char *strs[0] = {};
+    char *ret = longestCommonPrefix_14(strs, 0);
+    EXPECT_STREQ(ret, "");
+    free(ret);
+}
+
+TEST(leetcode_14, normal) {
+    char *strs[2] = {"qwert", "qwesd"};
+    char *ret = longestCommonPrefix_14(strs, 2);
+    EXPECT_STREQ(ret, "qwe");
+    free(ret);
+}
