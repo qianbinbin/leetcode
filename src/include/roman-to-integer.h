bbd2188383b03f26be9150e35a162f8b75b70b3e
Binbin Qian <qianbinbin@hotmail.com>
2018-03-10 20:32:48 +0800

    13. Roman to Integer

---
+++ src/include/roman-to-integer.h
@@ -0,0 +1,11 @@
+#ifndef LEETCODE_ROMAN_TO_INTEGER_H
+#define LEETCODE_ROMAN_TO_INTEGER_H
+
+/**
+ * Given a roman numeral, convert it to an integer.
+ *
+ * Input is guaranteed to be within the range from 1 to 3999.
+ */
+int romanToInt_13(char *s);
+
+#endif //LEETCODE_ROMAN_TO_INTEGER_H
