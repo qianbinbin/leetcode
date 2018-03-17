f0c0572c2311926db8632e4d679798812653d1db
Binbin Qian <qianbinbin@hotmail.com>
2018-03-17 22:26:30 +0800

    58. Length of Last Word

---
+++ src/include/length-of-last-word.h
@@ -0,0 +1,18 @@
+#ifndef LEETCODE_LENGTH_OF_LAST_WORD_H
+#define LEETCODE_LENGTH_OF_LAST_WORD_H
+
+/**
+ * Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+ *
+ * If the last word does not exist, return 0.
+ *
+ * Note: A word is defined as a character sequence consists of non-space characters only.
+ *
+ * Example:
+ *
+ * Input: "Hello World"
+ * Output: 5
+ */
+int lengthOfLastWord_58(char *s);
+
+#endif //LEETCODE_LENGTH_OF_LAST_WORD_H
