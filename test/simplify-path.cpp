015c3f6c7f8389e28cf26d313095f8848f9bb5af
Binbin Qian <qianbinbin@hotmail.com>
2018-03-17 21:20:57 +0800

    71. Simplify Path

---
+++ test/simplify-path.cpp
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+#include <simplify-path.h>
+}
+
+TEST(leetcode_71, normal) {
+    char *ret = simplifyPath_71("/a/./b/../../c/");
+    EXPECT_STREQ(ret, "/c");
+    free(ret);
+    ret = simplifyPath_71("/home/");
+    EXPECT_STREQ(ret, "/home");
+    free(ret);
+    ret = simplifyPath_71("/../../..");
+    EXPECT_STREQ(ret, "/");
+    free(ret);
+    ret = simplifyPath_71("/home//foo/");
+    EXPECT_STREQ(ret, "/home/foo");
+    free(ret);
+}
