25588fb1bfd34343faeb2b1a5282ba4ca289480d
Binbin Qian <qianbinbin@hotmail.com>
2018-04-05 21:35:27 +0800

    112. Path Sum

---
+++ src/include/path-sum.h
@@ -0,0 +1,31 @@
+#ifndef LEETCODE_PATH_SUM_H
+#define LEETCODE_PATH_SUM_H
+
+#include <common.h>
+#include <stdbool.h>
+
+/**
+ * Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+ *
+ * For example:
+ * Given the below binary tree and sum = 22,
+ *
+ *               5
+ *              / \
+ *             4   8
+ *            /   / \
+ *           11  13  4
+ *          /  \      \
+ *         7    2      1
+ * return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+ *
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool hasPathSum_112(struct TreeNode *root, int sum);
+
+#endif //LEETCODE_PATH_SUM_H
