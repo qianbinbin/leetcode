96b0099195c17fb07013267514bcfc30d84f9d89
Binbin Qian <qianbinbin@hotmail.com>
2018-04-06 21:25:54 +0800

    88. Merge Sorted Array - approach 1

---
+++ src/include/merge-sorted-array.h
@@ -0,0 +1,13 @@
+#ifndef LEETCODE_MERGE_SORTED_ARRAY_H
+#define LEETCODE_MERGE_SORTED_ARRAY_H
+
+/**
+ * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+ *
+ * Note:
+ * You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2. The number of elements initialized in nums1 and nums2 are m and n respectively.
+ */
+
+void merge_88_1(int *nums1, int m, int *nums2, int n);
+
+#endif //LEETCODE_MERGE_SORTED_ARRAY_H
