79e9e3756144bd5542f7acd447f0f7b8d11905c7
Binbin Qian <qianbinbin@hotmail.com>
2018-04-09 22:08:44 +0800

    41. First Missing Positive

---
+++ src/include/first-missing-positive.h
@@ -0,0 +1,15 @@
+#ifndef LEETCODE_FIRST_MISSING_POSITIVE_H
+#define LEETCODE_FIRST_MISSING_POSITIVE_H
+
+/**
+ * Given an unsorted integer array, find the first missing positive integer.
+ *
+ * For example,
+ * Given [1,2,0] return 3,
+ * and [3,4,-1,1] return 2.
+ *
+ * Your algorithm should run in O(n) time and uses constant space.
+ */
+int firstMissingPositive_41(int *nums, int numsSize);
+
+#endif //LEETCODE_FIRST_MISSING_POSITIVE_H
---
+++ test/first-missing-positive.cpp
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+#include <common.h>
+#include <first-missing-positive.h>
+}
+
+TEST(leetcode_41, normal_1) {
+    int nums[] = {1, 1};
+    array_print(nums, 2);
+    EXPECT_EQ(firstMissingPositive_41(nums, 2), 2);
+    array_print(nums, 2);
+}
+
+TEST(leetcode_41, normal_2) {
+    int nums[] = {3, 4, -1, 1};
+    array_print(nums, 4);
+    EXPECT_EQ(firstMissingPositive_41(nums, 4), 2);
+    array_print(nums, 4);
+}
