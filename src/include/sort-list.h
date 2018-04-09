d0eb2afefc02bbbc37774f0ec95a15d184e8a9c5
Binbin Qian <qianbinbin@hotmail.com>
2018-04-09 19:38:09 +0800

    148. Sort List

---
+++ src/include/sort-list.h
@@ -0,0 +1,17 @@
+#ifndef LEETCODE_SORT_LIST_H
+#define LEETCODE_SORT_LIST_H
+
+#include <common.h>
+
+/**
+ * Sort a linked list in O(n log n) time using constant space complexity.
+ *
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode *sortList_148(struct ListNode *head);
+
+#endif //LEETCODE_SORT_LIST_H
---
+++ test/sort-list.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+#include <sort-list.h>
+}
+
+TEST(leetcode_148, normal) {
+    int nums[] = {7, 5, 1, 3, 5, 9, 1, 2};
+    struct ListNode *list = list_from_array(nums, 8);
+    list_print(list);
+    list = sortList_148(list);
+    list_print(list);
+    list_free(list);
+}
