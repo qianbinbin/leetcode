9054f037f7ddee9a7998ec7c86e7382ed37b8ea7
Binbin Qian <qianbinbin@hotmail.com>
2018-04-19 23:44:32 +0800

    130. Surrounded Regions - approach 1: depth-first search

---
+++ src/include/surrounded-regions.h
@@ -0,0 +1,29 @@
+#ifndef LEETCODE_SURROUNDED_REGIONS_H
+#define LEETCODE_SURROUNDED_REGIONS_H
+
+/**
+ * Given a 2D board containing 'X' and 'O' (the letter O), capture all regions surrounded by 'X'.
+ *
+ * A region is captured by flipping all 'O's into 'X's in that surrounded region.
+ *
+ * For example,
+ *
+ * X X X X
+ * X O O X
+ * X X O X
+ * X O X X
+ *
+ * After running your function, the board should be:
+ *
+ * X X X X
+ * X X X X
+ * X X X X
+ * X O X X
+ */
+
+/**
+ * Depth-first search
+ */
+void solve_130_1(char **board, int boardRowSize, int boardColSize);
+
+#endif //LEETCODE_SURROUNDED_REGIONS_H
---
+++ src/surrounded-regions.c
@@ -0,0 +1,40 @@
+#include <surrounded-regions.h>
+#include <stdlib.h>
+
+static void mark_dfs(char **board, int i, int j, int row, int col) {
+    if (board[i][j] == 'O') {
+        board[i][j] = 'M';
+        if (i > 0)
+            mark_dfs(board, i - 1, j, row, col);
+        if (i < row - 1)
+            mark_dfs(board, i + 1, j, row, col);
+        if (j > 0)
+            mark_dfs(board, i, j - 1, row, col);
+        if (j < col - 1)
+            mark_dfs(board, i, j + 1, row, col);
+
+    }
+}
+
+void solve_130_1(char **board, int boardRowSize, int boardColSize) {
+    if (board == NULL || boardRowSize < 1 || boardColSize < 1) return;
+
+    for (int i = 0; i < boardRowSize; ++i) {
+        mark_dfs(board, i, 0, boardRowSize, boardColSize);
+        if (boardColSize > 1)
+            mark_dfs(board, i, boardColSize - 1, boardRowSize, boardColSize);
+    }
+    for (int j = 1; j < boardColSize - 1; ++j) {
+        mark_dfs(board, 0, j, boardRowSize, boardColSize);
+        if (boardRowSize > 1)
+            mark_dfs(board, boardRowSize - 1, j, boardRowSize, boardColSize);
+    }
+    for (int i = 0; i < boardRowSize; ++i) {
+        for (int j = 0; j < boardColSize; ++j) {
+            if (board[i][j] == 'O')
+                board[i][j] = 'X';
+            else if (board[i][j] == 'M')
+                board[i][j] = 'O';
+        }
+    }
+}
