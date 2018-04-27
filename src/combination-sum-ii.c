9befffe7072a7b4828d9ee1ed196eafdb996e26a
Binbin Qian <qianbinbin@hotmail.com>
2018-04-28 00:04:12 +0800

    40. Combination Sum II

---
+++ src/combination-sum-ii.c
@@ -0,0 +1,69 @@
+#include <combination-sum-ii.h>
+#include <stdlib.h>
+#include <string.h>
+
+static void combination_sum_dfs(int *sorted_candidates, int c_size, int index, int target, int sum,
+                                int ***combinations, int *capacity, int *size, int **col_sizes,
+                                int **path, int *path_capacity, int *path_size) {
+    if (sum == target) {
+        int *new_comb = (int *) malloc(*path_size * sizeof(int));
+        memcpy(new_comb, *path, *path_size * sizeof(int));
+        if (*size > *capacity) {
+            *capacity *= 2;
+            *combinations = (int **) realloc(*combinations, *capacity * sizeof(int *));
+            *col_sizes = (int *) realloc(*col_sizes, *capacity * sizeof(int));
+        }
+        (*combinations)[*size] = new_comb;
+        (*col_sizes)[*size] = *path_size;
+        ++(*size);
+        return;
+    }
+    if (sum > target) return;
+    int prev = -1;
+    for (int i = index; i < c_size; ++i) {
+        if (sorted_candidates[i] == prev) continue;
+        prev = sorted_candidates[i];
+        if (*path_size >= *path_capacity) {
+            *path_capacity *= 2;
+            *path = (int *) realloc(*path, *path_capacity * sizeof(int));
+        }
+        (*path)[(*path_size)++] = sorted_candidates[i];
+        combination_sum_dfs(sorted_candidates, c_size, i + 1, target, sum + sorted_candidates[i],
+                            combinations, capacity, size, col_sizes,
+                            path, path_capacity, path_size);
+        --(*path_size);
+    }
+}
+
+static int compare(const void *a, const void *b) {
+    return *(int *) a - *(int *) b;
+}
+
+int **combinationSum2_40(int *candidates, int candidatesSize, int target, int **columnSizes, int *returnSize) {
+    if (candidates == NULL || candidatesSize < 1 || target < 1 || columnSizes == NULL || returnSize == NULL)
+        return NULL;
+
+    int *nums = (int *) malloc(candidatesSize * sizeof(int));
+    memcpy(nums, candidates, candidatesSize * sizeof(int));
+    candidates = nums;
+    qsort(nums, candidatesSize, sizeof(int), compare);
+
+    int capacity = 64;
+    int **ret = (int **) malloc(capacity * sizeof(int *));
+    *returnSize = 0;
+    *columnSizes = (int *) malloc(capacity * sizeof(int));
+
+    int path_capacity = 64;
+    int *path = (int *) malloc(path_capacity * sizeof(int));
+    int path_size = 0;
+
+    combination_sum_dfs(candidates, candidatesSize, 0, target, 0,
+                        &ret, &capacity, returnSize, columnSizes,
+                        &path, &path_capacity, &path_size);
+
+    free(path);
+
+    ret = (int **) realloc(ret, *returnSize * sizeof(int *));
+    *columnSizes = (int *) realloc(*columnSizes, *returnSize * sizeof(int));
+    return ret;
+}
---
+++ src/include/combination-sum-ii.h
@@ -0,0 +1,40 @@
+#ifndef LEETCODE_COMBINATION_SUM_II_H
+#define LEETCODE_COMBINATION_SUM_II_H
+
+/**
+ * Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sums to target.
+ *
+ * Each number in candidates may only be used once in the combination.
+ *
+ * Note:
+ *
+ * All numbers (including target) will be positive integers.
+ * The solution set must not contain duplicate combinations.
+ *
+ * Example 1:
+ *
+ * Input: candidates = [10,1,2,7,6,1,5], target = 8,
+ * A solution set is:
+ * [
+ *   [1, 7],
+ *   [1, 2, 5],
+ *   [2, 6],
+ *   [1, 1, 6]
+ * ]
+ *
+ * Example 2:
+ *
+ * Input: candidates = [2,5,2,1,2], target = 5,
+ * A solution set is:
+ * [
+ *   [1,2,2],
+ *   [5]
+ * ]
+ *
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *columnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int **combinationSum2_40(int *candidates, int candidatesSize, int target, int **columnSizes, int *returnSize);
+
+#endif //LEETCODE_COMBINATION_SUM_II_H
