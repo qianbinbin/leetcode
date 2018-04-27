910643d021f085bd9e0ac89e24dacf9c7c6da29e
Binbin Qian <qianbinbin@hotmail.com>
2018-04-27 23:04:38 +0800

    39. Combination Sum

---
+++ src/include/combination-sum.h
@@ -0,0 +1,39 @@
+#ifndef LEETCODE_COMBINATION_SUM_H
+#define LEETCODE_COMBINATION_SUM_H
+
+/**
+ * Given a set of candidate numbers (candidates) (without duplicates) and a target number (target), find all unique combinations in candidates where the candidate numbers sums to target.
+ *
+ * The same repeated number may be chosen from candidates unlimited number of times.
+ *
+ * Note:
+ *
+ * All numbers (including target) will be positive integers.
+ * The solution set must not contain duplicate combinations.
+ *
+ * Example 1:
+ *
+ * Input: candidates = [2,3,6,7], target = 7,
+ * A solution set is:
+ * [
+ *   [7],
+ *   [2,2,3]
+ * ]
+ *
+ * Example 2:
+ *
+ * Input: candidates = [2,3,5], target = 8,
+ * A solution set is:
+ * [
+ *   [2,2,2,2],
+ *   [2,3,3],
+ *   [3,5]
+ * ]
+ *
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *columnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int **combinationSum_39(int *candidates, int candidatesSize, int target, int **columnSizes, int *returnSize);
+
+#endif //LEETCODE_COMBINATION_SUM_H
