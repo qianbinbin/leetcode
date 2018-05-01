be4d6ef13867ea4cdbca532d627749f4eb629795
Binbin Qian <qianbinbin@hotmail.com>
2018-05-01 23:23:07 +0800

    122. Best Time to Buy and Sell Stock II - approach 1

---
+++ src/best-time-to-buy-and-sell-stock-ii.c
@@ -0,0 +1,19 @@
+#include <best-time-to-buy-and-sell-stock-ii.h>
+#include <stddef.h>
+
+int maxProfit_122_1(int *prices, int pricesSize) {
+    if (prices == NULL || pricesSize < 0) return -1;
+    if (pricesSize < 2) return 0;
+
+    int i = 1;
+    int peak, valley;
+    int max_profit = 0;
+    while (i < pricesSize) {
+        while (i < pricesSize && prices[i] <= prices[i - 1]) ++i;
+        valley = prices[i - 1];
+        while (i < pricesSize && prices[i] >= prices[i - 1]) ++i;
+        peak = prices[i - 1];
+        max_profit += peak - valley;
+    }
+    return max_profit;
+}
