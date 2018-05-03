e798b3d11feb1d2dbe1655192ac73925f480e676
Binbin Qian <qianbinbin@hotmail.com>
2018-05-03 23:12:14 +0800

    85. Maximal Rectangle

---
+++ src/include/maximal-rectangle.h
@@ -0,0 +1,20 @@
+#ifndef LEETCODE_MAXIMAL_RECTANGLE_H
+#define LEETCODE_MAXIMAL_RECTANGLE_H
+
+/**
+ * Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
+ *
+ * Example:
+ *
+ * Input:
+ * [
+ *   ["1","0","1","0","0"],
+ *   ["1","0","1","1","1"],
+ *   ["1","1","1","1","1"],
+ *   ["1","0","0","1","0"]
+ * ]
+ * Output: 6
+ */
+int maximalRectangle_85(char **matrix, int matrixRowSize, int matrixColSize);
+
+#endif //LEETCODE_MAXIMAL_RECTANGLE_H
