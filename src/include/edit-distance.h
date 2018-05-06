addd306493610b5c503accc8099582a89e9783cc
Binbin Qian <qianbinbin@hotmail.com>
2018-05-06 19:50:52 +0800

    72. Edit Distance

---
+++ src/include/edit-distance.h
@@ -0,0 +1,35 @@
+#ifndef LEETCODE_EDIT_DISTANCE_H
+#define LEETCODE_EDIT_DISTANCE_H
+
+/**
+ * Given two words word1 and word2, find the minimum number of operations required to convert word1 to word2.
+ *
+ * You have the following 3 operations permitted on a word:
+ *
+ * Insert a character
+ * Delete a character
+ * Replace a character
+ *
+ * Example 1:
+ *
+ * Input: word1 = "horse", word2 = "ros"
+ * Output: 3
+ * Explanation:
+ * horse -> rorse (replace 'h' with 'r')
+ * rorse -> rose (remove 'r')
+ * rose -> ros (remove 'e')
+ *
+ * Example 2:
+ *
+ * Input: word1 = "intention", word2 = "execution"
+ * Output: 5
+ * Explanation:
+ * intention -> inention (remove 't')
+ * inention -> enention (replace 'i' with 'e')
+ * enention -> exention (replace 'n' with 'x')
+ * exention -> exection (replace 'n' with 'c')
+ * exection -> execution (insert 'u')
+ */
+int minDistance_72(char *word1, char *word2);
+
+#endif //LEETCODE_EDIT_DISTANCE_H
