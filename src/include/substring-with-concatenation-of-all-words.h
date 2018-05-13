0b8304e1cb661504af25b3ae4fed4bfe424dd179
Binbin Qian <qianbinbin@hotmail.com>
2018-05-13 22:30:57 +0800

    30. Substring with Concatenation of All Words

---
+++ src/include/substring-with-concatenation-of-all-words.h
@@ -0,0 +1,28 @@
+#ifndef LEETCODE_SUBSTRING_WITH_CONCATENATION_OF_ALL_WORDS_H
+#define LEETCODE_SUBSTRING_WITH_CONCATENATION_OF_ALL_WORDS_H
+
+/**
+ * You are given a string, s, and a list of words, words, that are all of the same length. Find all starting indices of substring(s) in s that is a concatenation of each word in words exactly once and without any intervening characters.
+ *
+ * Example 1:
+ *
+ * Input:
+ *   s = "barfoothefoobarman",
+ *   words = ["foo","bar"]
+ * Output: [0,9]
+ * Explanation: Substrings starting at index 0 and 9 are "barfoor" and "foobar" respectively.
+ * The output order does not matter, returning [9,0] is fine too.
+ *
+ * Example 2:
+ *
+ * Input:
+ *   s = "wordgoodstudentgoodword",
+ *   words = ["word","student"]
+ * Output: []
+ *
+ * Return an array of size *returnSize.
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int *findSubstring_30(char *s, char **words, int wordsSize, int *returnSize);
+
+#endif //LEETCODE_SUBSTRING_WITH_CONCATENATION_OF_ALL_WORDS_H
