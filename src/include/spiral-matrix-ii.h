dfb28d7ba6de8c5f0f82905f1a6578d29bb612fa
Binbin Qian <qianbinbin@hotmail.com>
2018-05-14 21:39:58 +0800

    59. Spiral Matrix II

---
+++ src/include/spiral-matrix-ii.h
@@ -0,0 +1,22 @@
+#ifndef LEETCODE_SPIRAL_MATRIX_II_H
+#define LEETCODE_SPIRAL_MATRIX_II_H
+
+/**
+ * Given a positive integer n, generate a square matrix filled with elements from 1 to n2 in spiral order.
+ *
+ * Example:
+ *
+ * Input: 3
+ * Output:
+ * [
+ *  [ 1, 2, 3 ],
+ *  [ 8, 9, 4 ],
+ *  [ 7, 6, 5 ]
+ * ]
+ *
+ * Return an array of arrays.
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int **generateMatrix_59(int n);
+
+#endif //LEETCODE_SPIRAL_MATRIX_II_H
