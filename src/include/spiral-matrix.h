90c32fa3954e52a79dc3f990eb4283fd4e8dd528
Binbin Qian <qianbinbin@hotmail.com>
2018-05-14 17:32:01 +0800

    54. Spiral Matrix

---
+++ src/include/spiral-matrix.h
@@ -0,0 +1,31 @@
+#ifndef LEETCODE_SPIRAL_MATRIX_H
+#define LEETCODE_SPIRAL_MATRIX_H
+
+/**
+ * Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+ *
+ * Example 1:
+ *
+ * Input:
+ * [
+ *  [ 1, 2, 3 ],
+ *  [ 4, 5, 6 ],
+ *  [ 7, 8, 9 ]
+ * ]
+ * Output: [1,2,3,6,9,8,7,4,5]
+ *
+ * Example 2:
+ *
+ * Input:
+ * [
+ *   [1, 2, 3, 4],
+ *   [5, 6, 7, 8],
+ *   [9,10,11,12]
+ * ]
+ * Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+ *
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int *spiralOrder_54(int **matrix, int matrixRowSize, int matrixColSize);
+
+#endif //LEETCODE_SPIRAL_MATRIX_H
