4f2e70f9129133d53a154f72fdf39d04a5c4c2da
Binbin Qian <qianbinbin@hotmail.com>
2018-05-23 16:15:33 +0800

    [C] Refactor 8. String to Integer (atoi)

--- src/main/c/string_to_integer_atoi.c
+++ src/main/c/string_to_integer_atoi.c
@@ -1,13 +1,14 @@
-#include <string_to_integer_atoi.h>
+#include "string_to_integer_atoi.h"
+
 #include <limits.h>
 #include <string.h>
 
 #define IS_NUMERIC(c) ('0' <= (c) && (c) <= '9')
 
-int myAtoi_8(char *str) {
+int myAtoi_8_1(char *str) {
     if (str == NULL) return 0;
 
-    const int len = strlen(str);
+    const size_t len = strlen(str);
     int i = 0;
     while (i < len && str[i] == ' ') ++i;
     if (i == len) return 0;
@@ -19,14 +20,14 @@ int myAtoi_8(char *str) {
         ++i;
     }
 
-    int num = 0;
+    int64_t num = 0;
     while (i < len && IS_NUMERIC(str[i])) {
-        if (num > INT_MAX / 10 ||
-            (num == INT_MAX / 10 && (str[i] - '0') > INT_MAX % 10)) {
-            return sign == 1 ? INT_MAX : INT_MIN;
-        }
         num = num * 10 + (str[i] - '0');
+        if (num > INT_MAX)
+            break;
         ++i;
     }
-    return sign * num;
+    if (num > INT_MAX)
+        return sign == 1 ? INT_MAX : INT_MIN;
+    return (int) (sign * num);
 }
--- src/test/c/string_to_integer_atoi_test.cpp
+++ src/test/c/string_to_integer_atoi_test.cpp
@@ -1,20 +1,13 @@
 #include <gtest/gtest.h>
 
 extern "C" {
-#include <string_to_integer_atoi.h>
+#include "string_to_integer_atoi.h"
 }
 
-TEST(leetcode_8, normal) {
-    EXPECT_EQ(myAtoi_8(""), 0);
-    EXPECT_EQ(myAtoi_8("-02194"), -2194);
-    EXPECT_EQ(myAtoi_8("   +00002.5"), 2);
-}
-
-TEST(leetcode_8, overflow) {
-    EXPECT_EQ(myAtoi_8("2147483646"), 2147483646);
-    EXPECT_EQ(myAtoi_8("2147483647"), INT_MAX);
-    EXPECT_EQ(myAtoi_8("2147483648"), INT_MAX);
-    EXPECT_EQ(myAtoi_8("-2147483647"), -2147483647);
-    EXPECT_EQ(myAtoi_8("-2147483648"), INT_MIN);
-    EXPECT_EQ(myAtoi_8("-2147483649"), INT_MIN);
+TEST(string_to_integer_atoi_test, myAtoi_8_1) {
+    EXPECT_EQ(myAtoi_8_1("42"), 42);
+    EXPECT_EQ(myAtoi_8_1("-42"), -42);
+    EXPECT_EQ(myAtoi_8_1("4193 with words"), 4193);
+    EXPECT_EQ(myAtoi_8_1("words and 987"), 0);
+    EXPECT_EQ(myAtoi_8_1("-91283472332"), -2147483648);
 }
