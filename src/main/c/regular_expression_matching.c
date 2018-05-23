9e82a9b5eeabc4005f750c6f008fac9da1ef352d
Binbin Qian <qianbinbin@hotmail.com>
2018-05-23 23:22:23 +0800

    [C] Refactor 10. Regular Expression Matching - approach 1: recursion

--- src/main/c/regular_expression_matching.c
+++ src/main/c/regular_expression_matching.c
@@ -1,15 +1,19 @@
-#include <regular_expression_matching.h>
-#include <string.h>
+#include "regular_expression_matching.h"
 
-bool isMatch_10(char *s, char *p) {
-    if (s == NULL || p == NULL) return false;
+#define MATCH(s, p) ((p) == '.' || (s) == (p))
+
+bool isMatch_10_1(char *s, char *p) {
     if (*p == '\0') return *s == '\0';
 
     if (*(p + 1) != '*') {
-        if (*s != '\0' && (*p == '.' || *s == *p)) return isMatch_10(s + 1, p + 1);
-        else return false;
+        if (*s != '\0' && MATCH(*s, *p))
+            return isMatch_10_1(s + 1, p + 1);
+        else
+            return false;
     } else {
-        if (*s != '\0' && (*p == '.' || *s == *p)) return isMatch_10(s, p + 2) || isMatch_10(s + 1, p);
-        else return isMatch_10(s, p + 2);
+        if (*s != '\0' && MATCH(*s, *p))
+            return isMatch_10_1(s, p + 2) || isMatch_10_1(s + 1, p);
+        else
+            return isMatch_10_1(s, p + 2);
     }
 }
--- src/test/c/regular_expression_matching_test.cpp
+++ src/test/c/regular_expression_matching_test.cpp
@@ -1,14 +1,13 @@
 #include <gtest/gtest.h>
 
 extern "C" {
-#include <regular_expression_matching.h>
+#include "regular_expression_matching.h"
 }
 
-TEST(leetcode_10, normal) {
-    EXPECT_FALSE(isMatch_10("aa", "a"));
-    EXPECT_TRUE(isMatch_10("aa", "aa"));
-    EXPECT_FALSE(isMatch_10("aaa", "aa"));
-    EXPECT_TRUE(isMatch_10("aa", "a*"));
-    EXPECT_TRUE(isMatch_10("aa", ".*"));
-    EXPECT_TRUE(isMatch_10("ab", ".*"));
+TEST(regular_expression_matching_test, isMatch_10_1) {
+    EXPECT_FALSE(isMatch_10_1("aa", "a"));
+    EXPECT_TRUE(isMatch_10_1("aa", "a*"));
+    EXPECT_TRUE(isMatch_10_1("ab", ".*"));
+    EXPECT_TRUE(isMatch_10_1("aab", "c*a*b"));
+    EXPECT_FALSE(isMatch_10_1("mississippi", "mis*is*p*."));
 }
