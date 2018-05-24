5b4d2d9f6c35883b4fd03f8e91a3864db01ed61e
Binbin Qian <qianbinbin@hotmail.com>
2018-05-24 17:45:21 +0800

    [C] Refactor 12. Integer to Roman

--- src/main/c/integer_to_roman.c
+++ src/main/c/integer_to_roman.c
@@ -1,20 +1,20 @@
-#include <integer_to_roman.h>
+#include "integer_to_roman.h"
+
 #include <stdlib.h>
 #include <string.h>
 
-char *intToRoman_12(int num) {
+char *intToRoman_12_1(int num) {
     if (num < 1 || num > 3999) return NULL;
 
     int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-    char *roman_numerals[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-    char str[20] = {0};
+    char *symbols[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    char *ret = (char *) calloc(20, sizeof(char));
     int idx = 0;
     while (num > 0) {
         while (values[idx] > num) ++idx;
-        strcat(str, roman_numerals[idx]);
+        strcat(ret, symbols[idx]);
         num -= values[idx];
     }
-    char *ret = (char *) malloc(strlen(str));
-    strcpy(ret, str);
+    ret = (char *) realloc(ret, strlen(ret) + 1);
     return ret;
 }
--- src/test/c/integer_to_roman_test.cpp
+++ src/test/c/integer_to_roman_test.cpp
@@ -1,11 +1,23 @@
 #include <gtest/gtest.h>
 
 extern "C" {
-#include <integer_to_roman.h>
+#include "integer_to_roman.h"
 }
 
-TEST(leetcode_12, normal) {
-    char *ret = intToRoman_12(386);
-    EXPECT_STREQ(ret, "CCCLXXXVI");
+TEST(integer_to_roman_test, intToRoman_12_1) {
+    char *ret = intToRoman_12_1(3);
+    EXPECT_STREQ(ret, "III");
+    free(ret);
+    ret = intToRoman_12_1(4);
+    EXPECT_STREQ(ret, "IV");
+    free(ret);
+    ret = intToRoman_12_1(9);
+    EXPECT_STREQ(ret, "IX");
+    free(ret);
+    ret = intToRoman_12_1(58);
+    EXPECT_STREQ(ret, "LVIII");
+    free(ret);
+    ret = intToRoman_12_1(1994);
+    EXPECT_STREQ(ret, "MCMXCIV");
     free(ret);
 }
