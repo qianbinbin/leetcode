7a3fff1cae0ca81407021df57652f90d425ee172
Binbin Qian <qianbinbin@hotmail.com>
2018-05-29 03:07:25 +0800

    [C] Refactor 22. Generate Parentheses

--- src/main/c/include/generate_parentheses.h
+++ src/main/c/include/generate_parentheses.h
@@ -1,7 +1,4 @@
-#ifndef LEETCODE_GENERATE_PARENTHESES_H
-#define LEETCODE_GENERATE_PARENTHESES_H
-
-/**
+/*
  * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
  *
  * For example, given n = 3, a solution set is:
@@ -13,13 +10,15 @@
  *   "()(())",
  *   "()()()"
  * ]
- *
+ */
+
+#ifndef LEETCODE_GENERATE_PARENTHESES_H
+#define LEETCODE_GENERATE_PARENTHESES_H
+
+/**
  * Return an array of size *returnSize.
  * Note: The returned array must be malloced, assume caller calls free().
  */
-
 char **generateParenthesis_22_1(int n, int *returnSize);
-
-char **generateParenthesis_22_2(int n, int *returnSize);
 
 #endif //LEETCODE_GENERATE_PARENTHESES_H
--- src/test/c/generate_parentheses_test.cpp
+++ src/test/c/generate_parentheses_test.cpp
@@ -1,22 +1,12 @@
 #include <gtest/gtest.h>
 
 extern "C" {
-#include <generate_parentheses.h>
+#include "generate_parentheses.h"
 }
 
-TEST(leetcode_22_1, normal) {
+TEST(generate_parentheses_test, generateParenthesis_22_1) {
     int size = 0;
     char **parenthesis = generateParenthesis_22_1(3, &size);
-    for (int i = 0; i < size; ++i) {
-        puts(parenthesis[i]);
-        free(parenthesis[i]);
-    }
-    free(parenthesis);
-}
-
-TEST(leetcode_22_2, normal) {
-    int size = 0;
-    char **parenthesis = generateParenthesis_22_2(3, &size);
     for (int i = 0; i < size; ++i) {
         puts(parenthesis[i]);
         free(parenthesis[i]);
