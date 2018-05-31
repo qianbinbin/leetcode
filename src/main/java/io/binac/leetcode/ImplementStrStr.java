b09acec6d6f43935c57d0a86917502328c5d0471
Binbin Qian <qianbinbin@hotmail.com>
2018-05-31 23:55:50 +0800

    [Java] 28. Implement strStr() - approach 2: KMP algorithm

--- src/main/java/io/binac/leetcode/ImplementStrStr.java
+++ src/main/java/io/binac/leetcode/ImplementStrStr.java
@@ -45,4 +45,56 @@ public class ImplementStrStr {
             return -1;
         }
     }
+
+    public static class Solution2 {
+        private int[] getNext(char pattern[]) {
+            final int len = pattern.length;
+            if (len == 0)
+                throw new IllegalArgumentException("pattern can't be empty");
+
+            int next[] = new int[len];
+            next[0] = -1;
+            if (len == 1)
+                return next;
+
+            next[1] = 0;
+            int i = 2, j = 0;
+            while (i < len) {
+                if (j == -1 || pattern[i - 1] == pattern[j]) {
+                    if (pattern[i] == pattern[j + 1])
+                        next[i++] = next[++j];
+                    else
+                        next[i++] = ++j;
+                } else {
+                    j = next[j];
+                }
+            }
+            return next;
+        }
+
+        public int strStr(String haystack, String needle) {
+            final int len1 = haystack.length(), len2 = needle.length();
+            if (len2 == 0)
+                return 0;
+            if (len1 == 0 || len1 < len2)
+                return -1;
+
+            char pattern[] = needle.toCharArray();
+            int next[] = getNext(pattern);
+
+            final int end = len1 - len2;
+            int i = 0, j = 0;
+            while (i < len1 && j < len2) {
+                if (j == -1 || haystack.charAt(i) == pattern[j]) {
+                    ++i;
+                    ++j;
+                } else {
+                    j = next[j];
+                }
+            }
+            if (j == len2)
+                return i - j;
+            return -1;
+        }
+    }
 }
--- src/test/java/io/binac/leetcode/ImplementStrStrTest.java
+++ src/test/java/io/binac/leetcode/ImplementStrStrTest.java
@@ -7,9 +7,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class ImplementStrStrTest {
     private final ImplementStrStr.Solution1 solution1 = new ImplementStrStr.Solution1();
 
+    private final ImplementStrStr.Solution2 solution2 = new ImplementStrStr.Solution2();
+
     @Test
     void test1() {
         assertEquals(2, solution1.strStr("hello", "ll"));
         assertEquals(-1, solution1.strStr("aaaaa", "bba"));
+    }
+
+    @Test
+    void test2() {
+        assertEquals(2, solution2.strStr("hello", "ll"));
+        assertEquals(-1, solution2.strStr("aaaaa", "bba"));
     }
 }
