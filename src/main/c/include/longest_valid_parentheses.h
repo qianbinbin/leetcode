82c6962ef0dfe1d43c2e1c2f5d603507a1947ef4
Binbin Qian <qianbinbin@hotmail.com>
2018-06-04 23:30:41 +0800

    [C] Refactor 32. Longest Valid Parentheses

--- src/main/c/include/longest_valid_parentheses.h
+++ src/main/c/include/longest_valid_parentheses.h
@@ -1,32 +1,35 @@
+/*
+ * Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+ *
+ * Example 1:
+ *
+ * Input: "(()"
+ * Output: 2
+ * Explanation: The longest valid parentheses substring is "()"
+ *
+ * Example 2:
+ *
+ * Input: ")()())"
+ * Output: 4
+ * Explanation: The longest valid parentheses substring is "()()"
+ */
+
 #ifndef LEETCODE_LONGEST_VALID_PARENTHESES_H
 #define LEETCODE_LONGEST_VALID_PARENTHESES_H
 
 /**
- * Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
- *
- * For "(()", the longest valid parentheses substring is "()", which has length = 2.
- *
- * Another example is ")()())", where the longest valid parentheses substring is "()()", which has length = 4.
- */
-
-/**
- * Brute force, O(n^2)
+ * Two passes
  */
 int longestValidParentheses_32_1(char *s);
 
 /**
- * Using stack, O(n)
+ * Using stack
  */
 int longestValidParentheses_32_2(char *s);
 
 /**
- * Dynamic programming, O(n)
+ * Dynamic programming
  */
 int longestValidParentheses_32_3(char *s);
-
-/**
- * Two scans, O(n)
- */
-int longestValidParentheses_32_4(char *s);
 
 #endif //LEETCODE_LONGEST_VALID_PARENTHESES_H
--- src/main/c/longest_valid_parentheses.c
+++ src/main/c/longest_valid_parentheses.c
@@ -1,39 +1,42 @@
-#include <longest_valid_parentheses.h>
-#include <stdbool.h>
+#include "longest_valid_parentheses.h"
+
 #include <string.h>
 #include <stdlib.h>
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
 
 int longestValidParentheses_32_1(char *s) {
     if (s == NULL) return 0;
 
+    int longest = 0;
     int top = -1;
-    int longest = 0, length = 0;
-    char *start = s;
-    while (true) {
-        if (top == -1) {
-            longest = length > longest ? length : longest;
-        }
-        if (*s == '(') {
+    int start = -1;
+    const int len = strlen(s);
+    for (int i = 0; i < len; ++i) {
+        if (s[i] == '(') {
             ++top;
-            ++length;
-            ++s;
-        } else if (*s == ')') {
+        } else {
             if (top == -1) {
-                length = 0;
-                start = s + 1;
+                start = i;
             } else {
                 --top;
-                ++length;
+                if (top == -1)
+                    longest = MAX(longest, i - start);
             }
-            ++s;
-        } else if (*s == '\0') {
+        }
+    }
+    top = -1;
+    start = len;
+    for (int i = len - 1; i >= 0; --i) {
+        if (s[i] == ')') {
+            ++top;
+        } else {
             if (top == -1) {
-                break;
+                start = i;
             } else {
-                length = 0;
-                top = -1;
-                ++start;
-                s = start;
+                --top;
+                if (top == -1)
+                    longest = MAX(longest, start - i);
             }
         }
     }
@@ -51,12 +54,12 @@ int longestValidParentheses_32_2(char *s) {
     for (int i = 0; i < len; ++i) {
         if (s[i] == '(') {
             stack[++top] = i;
-        } else if (s[i] == ')') {
+        } else {
             --top;
-            if (top == -1) stack[++top] = i;
-            else {
-                longest = (i - stack[top]) > longest ? (i - stack[top]) : longest;
-            }
+            if (top == -1)
+                stack[++top] = i;
+            else
+                longest = MAX(longest, i - stack[top]);
         }
     }
     free(stack);
@@ -79,52 +82,15 @@ int longestValidParentheses_32_3(char *s) {
                 dp[i] = dp[i - 2] + 2;
             else if (s[i - 1] == ')') {
                 j = i - dp[i - 1] - 1;
-                if (j >= 0 && s[j] == '(') {
+                if (j >= 0 && s[j] == '(')
                     dp[i] = dp[i - 1] + 2 + (j > 0 ? dp[j - 1] : 0);
-                } else dp[i] = 0;
+                else
+                    dp[i] = 0;
             }
-        } else dp[i] = 0;
-        longest = dp[i] > longest ? dp[i] : longest;
+            longest = MAX(longest, dp[i]);
+        } else
+            dp[i] = 0;
     }
-    return longest;
-}
-
-int longestValidParentheses_32_4(char *s) {
-    if (s == NULL) return 0;
-
-    int longest = 0;
-    int top = -1;
-    int start = -1;
-    const int len = strlen(s);
-    for (int i = 0; i < len; ++i) {
-        if (s[i] == '(') {
-            ++top;
-        } else if (s[i] == ')') {
-            if (top == -1) {
-                start = i;
-            } else {
-                --top;
-                if (top == -1) {
-                    longest = (i - start) > longest ? (i - start) : longest;
-                }
-            }
-        }
-    }
-    top = -1;
-    start = len;
-    for (int i = len - 1; i >= 0; --i) {
-        if (s[i] == ')') {
-            ++top;
-        } else if (s[i] == '(') {
-            if (top == -1) {
-                start = i;
-            } else {
-                --top;
-                if (top == -1) {
-                    longest = (start - i) > longest ? (start - i) : longest;
-                }
-            }
-        }
-    }
+    free(dp);
     return longest;
 }
