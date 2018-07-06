6741d7c144d04e49b9844ac6b7616d409c810849
Binbin Qian <qianbinbin@hotmail.com>
2018-07-06 23:59:04 +0800

    [C] Refactor 78. Subsets

--- src/main/c/include/subsets.h
+++ src/main/c/include/subsets.h
@@ -1,14 +1,12 @@
-#ifndef LEETCODE_SUBSETS_H
-#define LEETCODE_SUBSETS_H
-
-/**
+/*
  * Given a set of distinct integers, nums, return all possible subsets (the power set).
  *
  * Note: The solution set must not contain duplicate subsets.
  *
- * For example,
- * If nums = [1,2,3], a solution is:
+ * Example:
  *
+ * Input: nums = [1,2,3]
+ * Output:
  * [
  *   [3],
  *   [1],
@@ -19,11 +17,16 @@
  *   [1,2],
  *   []
  * ]
- *
+ */
+
+#ifndef LEETCODE_SUBSETS_H
+#define LEETCODE_SUBSETS_H
+
+/**
  * Return an array of arrays of size *returnSize.
  * The sizes of the arrays are returned as *columnSizes array.
  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
  */
-int **subsets_78(int *nums, int numsSize, int **columnSizes, int *returnSize);
+int **subsets_78_1(int *nums, int numsSize, int **columnSizes, int *returnSize);
 
 #endif //LEETCODE_SUBSETS_H
--- src/test/c/subsets_test.cpp
+++ src/test/c/subsets_test.cpp
@@ -1,15 +1,15 @@
 #include <gtest/gtest.h>
 
 extern "C" {
-#include <common.h>
-#include <subsets.h>
+#include "common.h"
+#include "subsets.h"
 }
 
-TEST(leetcode_78, normal) {
-    int nums[] = {1,2,3};
+TEST(subsets_test, subsets_78_1) {
+    int nums[] = {1, 2, 3};
     int *cols = NULL;
     int size = 0;
-    int **subsets = subsets_78(nums, 3, &cols, &size);
+    int **subsets = subsets_78_1(nums, 3, &cols, &size);
     for (int i = 0; i < size; ++i) {
         array_print(subsets[i], cols[i]);
         free(subsets[i]);
