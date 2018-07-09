243d710489d033d9d58bec2f0f9c15dc1f7cc594
Binbin Qian <qianbinbin@hotmail.com>
2018-07-09 16:25:07 +0800

    [Java] 85. Maximal Rectangle - approach 2: using stack

--- src/main/java/io/binac/leetcode/MaximalRectangle.java
+++ src/main/java/io/binac/leetcode/MaximalRectangle.java
@@ -1,6 +1,7 @@
 package io.binac.leetcode;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 /**
  * Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
@@ -48,6 +49,36 @@ public class MaximalRectangle {
                         end[j] = col;
                         e = j;
                     }
+                }
+            }
+            return max;
+        }
+    }
+
+    public static class Solution2 {
+        public int maximalRectangle(char[][] matrix) {
+            final int row = matrix.length;
+            if (row == 0) return 0;
+            final int col = matrix[0].length;
+
+            int max = 0;
+            int heights[] = new int[col];
+            Stack<Integer> stack = new Stack<>();
+            for (int i = 0; i < row; ++i) {
+                for (int j = 0; j < col; ++j) {
+                    if (matrix[i][j] == '1')
+                        ++heights[j];
+                    else
+                        heights[j] = 0;
+                    while (!stack.empty() && heights[stack.peek()] > heights[j]) {
+                        int index = stack.pop();
+                        max = Math.max(max, heights[index] * (stack.empty() ? j : j - stack.peek() - 1));
+                    }
+                    stack.push(j);
+                }
+                while (!stack.empty()) {
+                    int index = stack.pop();
+                    max = Math.max(max, heights[index] * (stack.empty() ? col : col - stack.peek() - 1));
                 }
             }
             return max;
--- src/test/java/io/binac/leetcode/MaximalRectangleTest.java
+++ src/test/java/io/binac/leetcode/MaximalRectangleTest.java
@@ -7,6 +7,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class MaximalRectangleTest {
     private final MaximalRectangle.Solution1 solution1 = new MaximalRectangle.Solution1();
 
+    private final MaximalRectangle.Solution2 solution2 = new MaximalRectangle.Solution2();
+
     @Test
     void test1() {
         char matrix[][] = {
@@ -16,5 +18,17 @@ class MaximalRectangleTest {
                 {'1', '0', '0', '1', '0'}
         };
         assertEquals(6, solution1.maximalRectangle(matrix));
+    }
+
+
+    @Test
+    void test2() {
+        char matrix[][] = {
+                {'1', '0', '1', '0', '0'},
+                {'1', '0', '1', '1', '1'},
+                {'1', '1', '1', '1', '1'},
+                {'1', '0', '0', '1', '0'}
+        };
+        assertEquals(6, solution2.maximalRectangle(matrix));
     }
 }
