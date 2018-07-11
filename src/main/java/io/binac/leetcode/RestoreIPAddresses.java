838b1696aa86cd8222f173965473d390511541f6
Binbin Qian <qianbinbin@hotmail.com>
2018-07-11 22:15:47 +0800

    [Java] 93. Restore IP Addresses - approach 1: DFS

---
+++ src/main/java/io/binac/leetcode/RestoreIPAddresses.java
@@ -0,0 +1,55 @@
+package io.binac.leetcode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Given a string containing only digits, restore it by returning all possible valid IP address combinations.
+ * <p>
+ * <p>Example:
+ * <blockquote><pre>
+ *     Input: "25525511135"
+ *     Output: ["255.255.11.135", "255.255.111.35"]
+ * </blockquote></pre>
+ */
+public class RestoreIPAddresses {
+    public static class Solution1 {
+        private boolean isValidByte(char[] chars, int offset, int count) {
+            if (offset < 0 || count < 1 || count > 3 || offset + count > chars.length)
+                return false;
+            if (chars[offset] == '0')
+                return count == 1;
+            int val = 0;
+            for (int i = 0; i < count; ++i)
+                val = val * 10 + chars[offset + i] - '0';
+            return val < 256;
+        }
+
+        private void restoreIpAddresses(char[] chars, int offset, List<String> result, char[] path, int len, int level) {
+            if (level == 4 || offset == chars.length) {
+                if (level == 4 && offset == chars.length)
+                    result.add(new String(path, 0, len - 1));
+                return;
+            }
+            for (int count = 1; count <= 3; ++count) {
+                if (isValidByte(chars, offset, count)) {
+                    System.arraycopy(chars, offset, path, len, count);
+                    len += count;
+                    path[len++] = '.';
+                    restoreIpAddresses(chars, offset + count, result, path, len, level + 1);
+                    len -= count + 1;
+                }
+            }
+        }
+
+        public List<String> restoreIpAddresses(String s) {
+            if (s.length() < 4 || s.length() > 12)
+                return Collections.emptyList();
+            List<String> result = new ArrayList<>();
+            char path[] = new char[16];
+            restoreIpAddresses(s.toCharArray(), 0, result, path, 0, 0);
+            return result;
+        }
+    }
+}
---
+++ src/test/java/io/binac/leetcode/RestoreIPAddressesTest.java
@@ -0,0 +1,18 @@
+package io.binac.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestoreIPAddressesTest {
+    private final RestoreIPAddresses.Solution1 solution1 = new RestoreIPAddresses.Solution1();
+
+    @Test
+    void test1() {
+        List<String> expected = Arrays.asList("255.255.11.135", "255.255.111.35");
+        assertEquals(expected, solution1.restoreIpAddresses("25525511135"));
+    }
+}
