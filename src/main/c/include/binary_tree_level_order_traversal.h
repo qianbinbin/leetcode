09b20d166a5d850ee9804803d1891b2b27441687
Binbin Qian <qianbinbin@hotmail.com>
2018-07-15 22:17:51 +0800

    [C] Refactor 102. Binary Tree Level Order Traversal

--- src/main/c/include/binary_tree_level_order_traversal.h
+++ src/main/c/include/binary_tree_level_order_traversal.h
@@ -1,36 +1,36 @@
-#ifndef LEETCODE_BINARY_TREE_LEVEL_ORDER_TRAVERSAL_H
-#define LEETCODE_BINARY_TREE_LEVEL_ORDER_TRAVERSAL_H
-
-#include <common.h>
-
-/**
+/*
  * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
  *
  * For example:
  * Given binary tree [3,9,20,null,null,15,7],
+ *
  *     3
  *    / \
  *   9  20
  *     /  \
  *    15   7
+ *
  * return its level order traversal as:
+ *
  * [
  *   [3],
  *   [9,20],
  *   [15,7]
  * ]
- *
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     struct TreeNode *left;
- *     struct TreeNode *right;
- * };
- *
+ */
+
+#ifndef LEETCODE_BINARY_TREE_LEVEL_ORDER_TRAVERSAL_H
+#define LEETCODE_BINARY_TREE_LEVEL_ORDER_TRAVERSAL_H
+
+#include "common.h"
+
+/**
  * Return an array of arrays of size *returnSize.
  * The sizes of the arrays are returned as *columnSizes array.
  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
  */
-int **levelOrder_102(struct TreeNode *root, int **columnSizes, int *returnSize);
+int **levelOrder_102_1(struct TreeNode *root, int **columnSizes, int *returnSize);
+
+int **levelOrder_102_2(struct TreeNode *root, int **columnSizes, int *returnSize);
 
 #endif //LEETCODE_BINARY_TREE_LEVEL_ORDER_TRAVERSAL_H
--- src/test/c/binary_tree_level_order_traversal_test.cpp
+++ src/test/c/binary_tree_level_order_traversal_test.cpp
@@ -1,20 +1,35 @@
 #include <gtest/gtest.h>
 
 extern "C" {
-#include <binary_tree_level_order_traversal.h>
+#include "binary_tree_level_order_traversal.h"
 }
 
-TEST(leetcode_102, normal) {
+TEST(binary_tree_level_order_traversal_test, levelOrder_102_1) {
     int nums[] = {3, 9, 20, NTNODE, NTNODE, 15, 7};
-    struct TreeNode *tree = tree_create(nums, 7);
+    struct TreeNode *root = tree_create(nums, 7);
     int size = 0;
-    int *columnSizes = (int *) malloc(sizeof(int));
-    int **ret = levelOrder_102(tree, &columnSizes, &size);
+    int *columnSizes = NULL;
+    int **ret = levelOrder_102_1(root, &columnSizes, &size);
     for (int i = 0; i < size; ++i) {
         array_print(ret[i], columnSizes[i]);
         free(ret[i]);
     }
     free(ret);
     free(columnSizes);
-    tree_free(tree);
+    tree_free(root);
+}
+
+TEST(binary_tree_level_order_traversal_test, levelOrder_102_2) {
+    int nums[] = {3, 9, 20, NTNODE, NTNODE, 15, 7};
+    struct TreeNode *root = tree_create(nums, 7);
+    int size = 0;
+    int *columnSizes = NULL;
+    int **ret = levelOrder_102_2(root, &columnSizes, &size);
+    for (int i = 0; i < size; ++i) {
+        array_print(ret[i], columnSizes[i]);
+        free(ret[i]);
+    }
+    free(ret);
+    free(columnSizes);
+    tree_free(root);
 }
