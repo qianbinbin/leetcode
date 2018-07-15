7457fc8d413ffe14d30a6702e6fbd5b5f85cadf4
Binbin Qian <qianbinbin@hotmail.com>
2018-07-15 23:05:47 +0800

    [Java] 103. Binary Tree Zigzag Level Order Traversal - approach 2: level order traversal

--- src/main/java/io/binac/leetcode/BinaryTreeZigzagLevelOrderTraversal.java
+++ src/main/java/io/binac/leetcode/BinaryTreeZigzagLevelOrderTraversal.java
@@ -2,9 +2,7 @@ package io.binac.leetcode;
 
 import io.binac.leetcode.util.TreeNode;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
@@ -47,6 +45,32 @@ public class BinaryTreeZigzagLevelOrderTraversal {
         public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
             List<List<Integer>> result = new ArrayList<>();
             preOrder(root, 0, result);
+            return result;
+        }
+    }
+
+    public static class Solution2 {
+        public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+            if (root == null) return Collections.emptyList();
+
+            List<List<Integer>> result = new ArrayList<>();
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            boolean order = true;
+            while (!queue.isEmpty()) {
+                LinkedList<Integer> level = new LinkedList<>();
+                int size = queue.size();
+                for (int i = 0; i < size; ++i) {
+                    TreeNode n = queue.poll();
+                    assert n != null;
+                    if (order) level.addLast(n.val);
+                    else level.addFirst(n.val);
+                    if (n.left != null) queue.offer(n.left);
+                    if (n.right != null) queue.offer(n.right);
+                }
+                result.add(level);
+                order = !order;
+            }
             return result;
         }
     }
--- src/test/java/io/binac/leetcode/BinaryTreeZigzagLevelOrderTraversalTest.java
+++ src/test/java/io/binac/leetcode/BinaryTreeZigzagLevelOrderTraversalTest.java
@@ -12,6 +12,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class BinaryTreeZigzagLevelOrderTraversalTest {
     private final BinaryTreeZigzagLevelOrderTraversal.Solution1 solution1 = new BinaryTreeZigzagLevelOrderTraversal.Solution1();
 
+    private final BinaryTreeZigzagLevelOrderTraversal.Solution2 solution2 = new BinaryTreeZigzagLevelOrderTraversal.Solution2();
+
     @Test
     void test1() {
         List<List<Integer>> expected = Arrays.asList(
@@ -20,5 +22,15 @@ class BinaryTreeZigzagLevelOrderTraversalTest {
                 Arrays.asList(15, 7)
         );
         assertEquals(expected, solution1.zigzagLevelOrder(Trees.asTree(3, 9, 20, null, null, 15, 7)));
+    }
+
+    @Test
+    void test2() {
+        List<List<Integer>> expected = Arrays.asList(
+                Collections.singletonList(3),
+                Arrays.asList(20, 9),
+                Arrays.asList(15, 7)
+        );
+        assertEquals(expected, solution2.zigzagLevelOrder(Trees.asTree(3, 9, 20, null, null, 15, 7)));
     }
 }
