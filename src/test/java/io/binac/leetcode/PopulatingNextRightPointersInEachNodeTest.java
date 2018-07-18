e8f81e09641441ad24ff23768edf48c17f641cda
Binbin Qian <qianbinbin@hotmail.com>
2018-07-19 00:30:49 +0800

    [Java] 116. Populating Next Right Pointers in Each Node - approach 1: iteration

---
+++ src/test/java/io/binac/leetcode/PopulatingNextRightPointersInEachNodeTest.java
@@ -0,0 +1,14 @@
+package io.binac.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PopulatingNextRightPointersInEachNodeTest {
+    private final PopulatingNextRightPointersInEachNode.Solution1 solution1 = new PopulatingNextRightPointersInEachNode.Solution1();
+
+    @Test
+    void test1() {
+        // TODO
+    }
+}
