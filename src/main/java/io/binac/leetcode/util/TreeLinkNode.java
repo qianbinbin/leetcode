8c2a67123de345258705c779c2be2e88cf56edb2
Binbin Qian <qianbinbin@hotmail.com>
2018-07-19 00:30:25 +0800

    [Java] TreeLinkNode

---
+++ src/main/java/io/binac/leetcode/util/TreeLinkNode.java
@@ -0,0 +1,10 @@
+package io.binac.leetcode.util;
+
+public class TreeLinkNode {
+    public int val;
+    public TreeLinkNode left, right, next;
+
+    public TreeLinkNode(int x) {
+        val = x;
+    }
+}
