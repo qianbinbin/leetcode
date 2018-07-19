3c5a10220000e9463705680503d4cbd82f468873
Binbin Qian <qianbinbin@hotmail.com>
2018-07-19 17:21:52 +0800

    [Java] 116. Populating Next Right Pointers in Each Node - approach 2: recursion

--- src/main/java/io/binac/leetcode/PopulatingNextRightPointersInEachNode.java
+++ src/main/java/io/binac/leetcode/PopulatingNextRightPointersInEachNode.java
@@ -59,4 +59,17 @@ public class PopulatingNextRightPointersInEachNode {
             }
         }
     }
+
+    public static class Solution2 {
+        public void connect(TreeLinkNode root) {
+            if (root == null) return;
+            if (root.left != null) {
+                root.left.next = root.right;
+                if (root.next != null)
+                    root.right.next = root.next.left;
+            }
+            connect(root.left);
+            connect(root.right);
+        }
+    }
 }
--- src/test/java/io/binac/leetcode/PopulatingNextRightPointersInEachNodeTest.java
+++ src/test/java/io/binac/leetcode/PopulatingNextRightPointersInEachNodeTest.java
@@ -7,8 +7,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class PopulatingNextRightPointersInEachNodeTest {
     private final PopulatingNextRightPointersInEachNode.Solution1 solution1 = new PopulatingNextRightPointersInEachNode.Solution1();
 
+    private final PopulatingNextRightPointersInEachNode.Solution2 solution2 = new PopulatingNextRightPointersInEachNode.Solution2();
+
     @Test
     void test1() {
+        // TODO
+    }
+
+    @Test
+    void test2() {
         // TODO
     }
 }
