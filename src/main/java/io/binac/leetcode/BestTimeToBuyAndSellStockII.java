4bb5405702259bd62c3d6137b0d09fd5d8c31ed5
Binbin Qian <qianbinbin@hotmail.com>
2018-07-21 17:30:02 +0800

    [Java] 122. Best Time to Buy and Sell Stock II - approach 2: greedy

--- src/main/java/io/binac/leetcode/BestTimeToBuyAndSellStockII.java
+++ src/main/java/io/binac/leetcode/BestTimeToBuyAndSellStockII.java
@@ -44,4 +44,15 @@ public class BestTimeToBuyAndSellStockII {
             return result;
         }
     }
+
+    public static class Solution2 {
+        public int maxProfit(int[] prices) {
+            int result = 0;
+            for (int i = 1; i < prices.length; ++i) {
+                if (prices[i] > prices[i - 1])
+                    result += prices[i] - prices[i - 1];
+            }
+            return result;
+        }
+    }
 }
--- src/test/java/io/binac/leetcode/BestTimeToBuyAndSellStockIITest.java
+++ src/test/java/io/binac/leetcode/BestTimeToBuyAndSellStockIITest.java
@@ -7,6 +7,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class BestTimeToBuyAndSellStockIITest {
     private final BestTimeToBuyAndSellStockII.Solution1 solution1 = new BestTimeToBuyAndSellStockII.Solution1();
 
+    private final BestTimeToBuyAndSellStockII.Solution2 solution2 = new BestTimeToBuyAndSellStockII.Solution2();
+
     @Test
     void test1() {
         int prices1[] = {7, 1, 5, 3, 6, 4};
@@ -17,5 +19,17 @@ class BestTimeToBuyAndSellStockIITest {
 
         int prices3[] = {7, 6, 4, 3, 1};
         assertEquals(0, solution1.maxProfit(prices3));
+    }
+
+    @Test
+    void test2() {
+        int prices1[] = {7, 1, 5, 3, 6, 4};
+        assertEquals(7, solution2.maxProfit(prices1));
+
+        int prices2[] = {1, 2, 3, 4, 5};
+        assertEquals(4, solution2.maxProfit(prices2));
+
+        int prices3[] = {7, 6, 4, 3, 1};
+        assertEquals(0, solution2.maxProfit(prices3));
     }
 }
