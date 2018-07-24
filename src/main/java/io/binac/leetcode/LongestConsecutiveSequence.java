82cc04c6563704db0f544d396937033184c3f92b
Binbin Qian <qianbinbin@hotmail.com>
2018-07-24 19:20:44 +0800

    [Java] 128. Longest Consecutive Sequence - approach 2: union-find

--- src/main/java/io/binac/leetcode/LongestConsecutiveSequence.java
+++ src/main/java/io/binac/leetcode/LongestConsecutiveSequence.java
@@ -1,6 +1,8 @@
 package io.binac.leetcode;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -31,6 +33,68 @@ public class LongestConsecutiveSequence {
                 }
             }
             return result;
+        }
+    }
+
+    public static class Solution2 {
+        private static class UnionFind {
+            private final int parent[];
+            private final int rank[];
+
+            public UnionFind(int n) {
+                if (n <= 0)
+                    throw new IllegalArgumentException();
+                parent = new int[n];
+                for (int i = 0; i < n; ++i) parent[i] = i;
+                rank = new int[n];
+            }
+
+            public void union(int a, int b) {
+                int root1 = find(a), root2 = find(b);
+                if (root1 == root2)
+                    return;
+                if (rank[root1] > rank[root2]) {
+                    parent[root2] = root1;
+                } else {
+                    parent[root1] = root2;
+                    if (rank[root1] == rank[root2])
+                        ++rank[root2];
+                }
+            }
+
+            public int find(int i) {
+                if (i == parent[i])
+                    return i;
+                parent[i] = find(parent[i]);
+                return parent[i];
+            }
+
+            public int getMaxSetSize() {
+                int result = 1;
+                int size[] = new int[parent.length];
+                for (int i = 0; i < parent.length; ++i) {
+                    int root = find(i);
+                    ++size[root];
+                    result = Math.max(result, size[root]);
+                }
+                return result;
+            }
+        }
+
+        public int longestConsecutive(int[] nums) {
+            if (nums.length == 0) return 0;
+
+            Map<Integer, Integer> map = new HashMap<>();
+            UnionFind uf = new UnionFind(nums.length);
+            for (int i = 0; i < nums.length; ++i) {
+                if (map.containsKey(nums[i])) continue;
+                map.put(nums[i], i);
+                Integer left = map.get(nums[i] - 1);
+                if (left != null) uf.union(i, left);
+                Integer right = map.get(nums[i] + 1);
+                if (right != null) uf.union(i, right);
+            }
+            return uf.getMaxSetSize();
         }
     }
 }
--- src/test/java/io/binac/leetcode/LongestConsecutiveSequenceTest.java
+++ src/test/java/io/binac/leetcode/LongestConsecutiveSequenceTest.java
@@ -7,9 +7,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class LongestConsecutiveSequenceTest {
     private final LongestConsecutiveSequence.Solution1 solution1 = new LongestConsecutiveSequence.Solution1();
 
+    private final LongestConsecutiveSequence.Solution2 solution2 = new LongestConsecutiveSequence.Solution2();
+
     @Test
     void test1() {
         int nums[] = {100, 4, 200, 1, 3, 2};
         assertEquals(4, solution1.longestConsecutive(nums));
+    }
+
+    @Test
+    void test2() {
+        int nums[] = {100, 4, 200, 1, 3, 2};
+        assertEquals(4, solution2.longestConsecutive(nums));
     }
 }
