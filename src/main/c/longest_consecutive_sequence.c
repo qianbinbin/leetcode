b7250e8e04189e351c02a00a65601e201176b485
Binbin Qian <qianbinbin@hotmail.com>
2018-07-24 21:55:57 +0800

    [C] Refactor 128. Longest Consecutive Sequence

--- src/main/c/longest_consecutive_sequence.c
+++ src/main/c/longest_consecutive_sequence.c
@@ -1,142 +1,185 @@
-#include <assert.h>
-#include <string.h>
+#include "longest_consecutive_sequence.h"
+
+#include <stdbool.h>
 #include <stdlib.h>
-#include <longest_consecutive_sequence.h>
+#include <string.h>
 
-// Approach 1: sorting, O(nlog(n)) but accepted
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
 
-int compare(const void *a, const void *b) {
-    return *(int *) a - *(int *) b;
+#define HASH_MAP_SIZE 256
+
+typedef struct Entry {
+    int key;
+    int value;
+    struct Entry *next;
+} entry;
+
+typedef struct HashMap {
+    entry entries[HASH_MAP_SIZE];
+} hashmap;
+
+static int hashcode(int key) {
+    return key;
+}
+
+static hashmap *hashmap_create() {
+    hashmap *map = (hashmap *) malloc(sizeof(hashmap));
+    entry *entry = map->entries;
+    for (int i = 0; i < HASH_MAP_SIZE; ++i)
+        entry++->next = NULL;
+    return map;
+}
+
+static void hashmap_put(hashmap *map, int key, int value) {
+    int index = hashcode(key) & (HASH_MAP_SIZE - 1);
+    entry *e = map->entries + index;
+    while (e->next != NULL) {
+        if (e->next->key == key) {
+            e->next->value = value;
+            return;
+        }
+        e = e->next;
+    }
+    entry *new = (entry *) malloc(sizeof(entry));
+    new->key = key;
+    new->value = value;
+    new->next = NULL;
+    e->next = new;
+}
+
+static void hashmap_get(hashmap *map, int key, int *value) {
+    int index = hashcode(key) & (HASH_MAP_SIZE - 1);
+    entry *e = map->entries + index;
+    while (e->next != NULL) {
+        if (e->next->key == key) {
+            *value = e->next->value;
+            return;
+        }
+        e = e->next;
+    }
+}
+
+static bool hashmap_contains(hashmap *map, int key) {
+    int index = hashcode(key) & (HASH_MAP_SIZE - 1);
+    entry *e = map->entries + index;
+    while (e->next != NULL) {
+        if (e->next->key == key)
+            return true;
+        e = e->next;
+    }
+    return false;
+}
+
+static void hashmap_free(hashmap *map) {
+    entry *e, *n, *tmp;
+    for (int i = 0; i < HASH_MAP_SIZE; ++i) {
+        e = map->entries + i;
+        n = e->next;
+        while (n != NULL) {
+            e->next = n->next;
+            tmp = n;
+            n = e->next;
+            free(tmp);
+        }
+    }
+    free(map);
 }
 
 int longestConsecutive_128_1(int *nums, int numsSize) {
     if (nums == NULL || numsSize < 0) return -1;
     if (numsSize < 2) return numsSize;
 
-    int *tmp = (int *) malloc(numsSize * sizeof(int));
-    assert(tmp != NULL);
-    memcpy(tmp, nums, numsSize * sizeof(int));
-    qsort(tmp, numsSize, sizeof(int), compare);
-    int length = 1, longest = 1;
-    for (int i = 1; i < numsSize; ++i) {
-        if (tmp[i] == tmp[i - 1]) {
-            continue;
-        } else if (tmp[i] == tmp[i - 1] + 1) {
-            ++length;
-        } else {
-            longest = longest > length ? longest : length;
-            length = 1;
+    int ret = 1;
+    hashmap *map = hashmap_create();
+    for (int i = 0; i < numsSize; ++i) hashmap_put(map, nums[i], i);
+
+    for (int i = 0; i < numsSize; ++i) {
+        if (!hashmap_contains(map, nums[i] - 1)) {
+            int num = nums[i] + 1;
+            while (hashmap_contains(map, num)) ++num;
+            ret = MAX(ret, num - nums[i]);
         }
     }
-    longest = longest > length ? longest : length;
-    free(tmp);
-    return longest;
+    hashmap_free(map);
+    return ret;
 }
 
-// Approach 2: hashmap, O(n)
+typedef struct UnionFind {
+    int n;
+    int *parent;
+    int *rank;
+} uf;
 
-#define HASH_MAP_SIZE 1024
-
-struct Entry {
-    int key;
-    int value;
-    struct Entry *next;
-} entries[HASH_MAP_SIZE];
-
-int hashcode(int key) {
-    return abs(key) % HASH_MAP_SIZE;
+static uf *uf_create(int n) {
+    uf *ret = (uf *) malloc(sizeof(uf));
+    ret->n = n;
+    int *parent = (int *) malloc(n * sizeof(int));
+    for (int i = 0; i < n; ++i) parent[i] = i;
+    ret->parent = parent;
+    ret->rank = (int *) calloc(n, sizeof(int));
+    return ret;
 }
 
-void hashmap_init() {
-    for (int i = 0; i < HASH_MAP_SIZE; ++i)
-        entries[i].next = NULL;
+static int uf_find(uf *u, int i) {
+    if (i == u->parent[i])
+        return i;
+    u->parent[i] = uf_find(u, u->parent[i]);
+    return u->parent[i];
 }
 
-int hashmap_put(int key, int value) {
-    struct Entry *p = entries + hashcode(key);
-    while (p->next != NULL) {
-        if (p->next->key == key) {
-            p->next->value = value;
-            return 0;
-        }
-        p = p->next;
+static void uf_union(uf *u, int a, int b) {
+    int root1 = uf_find(u, a), root2 = uf_find(u, b);
+    if (root1 == root2) return;
+    if (u->rank[root1] > u->rank[root2]) {
+        u->parent[root2] = root1;
+    } else {
+        u->parent[root1] = root2;
+        if (u->rank[root1] == u->rank[root2])
+            ++(u->rank[root2]);
     }
-
-    p->next = (struct Entry *) malloc(sizeof(struct Entry));
-    if (p->next != NULL) {
-        p->next->key = key;
-        p->next->value = value;
-        p->next->next = NULL;
-        return 0;
-    }
-    return -1;
 }
 
-int hashmap_get(int key, int *value) {
-    if (value == NULL) return -2;
-
-    struct Entry *p = entries + hashcode(key);
-    while (p->next != NULL) {
-        if (p->next->key == key) {
-            *value = p->next->value;
-            return 0;
-        }
-        p = p->next;
+static int uf_max_set_size(uf *u) {
+    if (u->n == 0) return 0;
+    int ret = 1;
+    int *size = (int *) calloc(u->n, sizeof(int));
+    for (int i = 0; i < u->n; ++i) {
+        int root = uf_find(u, i);
+        ++size[root];
+        ret = MAX(ret, size[root]);
     }
-    return -1;
+    return ret;
 }
 
-int hashmap_remove(int key) {
-    struct Entry *p = entries + hashcode(key);
-    while (p->next != NULL) {
-        if (p->next->key == key) {
-            struct Entry *q = p->next;
-            p->next = q->next;
-            free(q);
-            return 0;
-        }
-        p = p->next;
-    }
-    return -1;
-}
-
-void hashmap_uninit() {
-    struct Entry *p;
-    for (int i = 0; i < HASH_MAP_SIZE; ++i) {
-        while (entries[i].next != NULL) {
-            p = entries[i].next;
-            entries[i].next = p->next;
-            free(p);
-        }
-    }
+static void uf_free(uf *u) {
+    free(u->parent);
+    free(u->rank);
+    free(u);
 }
 
 int longestConsecutive_128_2(int *nums, int numsSize) {
     if (nums == NULL || numsSize < 0) return -1;
     if (numsSize < 2) return numsSize;
 
-    hashmap_init();
+    hashmap *map = hashmap_create();
+    uf *u = uf_create(numsSize);
     for (int i = 0; i < numsSize; ++i) {
-        hashmap_put(nums[i], 0);
-    }
-
-    int longest = 1, length;
-    int visited = 0;
-    int offset;
-    for (int i = 0; i < numsSize; ++i) {
-        hashmap_get(nums[i], &visited);
-        if (visited == 1) continue;
-        length = 1;
-        for (offset = 1; hashmap_get(nums[i] + offset, &visited) == 0; ++offset) {
-            hashmap_put(nums[i] + offset, 1);
-            ++length;
+        if (hashmap_contains(map, nums[i])) continue;
+        hashmap_put(map, nums[i], i);
+        int index = -1;
+        hashmap_get(map, nums[i] - 1, &index);
+        if (index != -1) {
+            uf_union(u, i, index);
+            index = -1;
         }
-        for (offset = -1; hashmap_get(nums[i] + offset, &visited) == 0; --offset) {
-            hashmap_put(nums[i] + offset, 1);
-            ++length;
+        hashmap_get(map, nums[i] + 1, &index);
+        if (index != -1) {
+            uf_union(u, i, index);
+            index = -1;
         }
-        longest = longest > length ? longest : length;
     }
-    hashmap_uninit();
-    return longest;
+    hashmap_free(map);
+    int ret = uf_max_set_size(u);
+    uf_free(u);
+    return ret;
 }
--- src/test/c/longest_consecutive_sequence_test.cpp
+++ src/test/c/longest_consecutive_sequence_test.cpp
@@ -1,30 +1,18 @@
 #include <gtest/gtest.h>
 
 extern "C" {
-#include <common.h>
-#include <longest_consecutive_sequence.h>
+#include "common.h"
+#include "longest_consecutive_sequence.h"
 }
 
-TEST(leetcode_128_1, empty) {
-    int nums[] = {};
-    array_print(nums, 0);
-    EXPECT_EQ(longestConsecutive_128_1(nums, 0), 0);
+#define ARR_SIZE(a) (sizeof(a) / sizeof((a)[0]))
+
+TEST(longest_consecutive_sequence_test, longestConsecutive_128_1) {
+    int nums[] = {100, 4, 200, 1, 3, 2};
+    EXPECT_EQ(longestConsecutive_128_1(nums, ARR_SIZE(nums)), 4);
 }
 
-TEST(leetcode_128_1, normal) {
-    int nums[] = {1, 4, 2, 1, 3, 2};
-    array_print(nums, 6);
-    EXPECT_EQ(longestConsecutive_128_1(nums, 6), 4);
-}
-
-TEST(leetcode_128_2, empty) {
-    int nums[] = {};
-    array_print(nums, 0);
-    EXPECT_EQ(longestConsecutive_128_2(nums, 0), 0);
-}
-
-TEST(leetcode_128_2, normal) {
-    int nums[] = {1, 4, 2, 1, 3, 2};
-    array_print(nums, 6);
-    EXPECT_EQ(longestConsecutive_128_2(nums, 6), 4);
+TEST(longest_consecutive_sequence_test, longestConsecutive_128_2) {
+    int nums[] = {100, 4, 200, 1, 3, 2};
+    EXPECT_EQ(longestConsecutive_128_2(nums, ARR_SIZE(nums)), 4);
 }
