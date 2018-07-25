11f53da08b36a3ce623bc97a94544ae5f118d78e
Binbin Qian <qianbinbin@hotmail.com>
2018-07-25 19:05:11 +0800

    [C] Refactor 131. Palindrome Partitioning

--- src/main/c/include/palindrome_partitioning.h
+++ src/main/c/include/palindrome_partitioning.h
@@ -1,7 +1,4 @@
-#ifndef LEETCODE_PALINDROME_PARTITIONING_H
-#define LEETCODE_PALINDROME_PARTITIONING_H
-
-/**
+/*
  * Given a string s, partition s such that every substring of the partition is a palindrome.
  *
  * Return all possible palindrome partitioning of s.
@@ -14,20 +11,11 @@
  *   ["aa","b"],
  *   ["a","a","b"]
  * ]
- *
- * Return an array of arrays of size *returnSize.
- * The sizes of the arrays are returned as *columnSizes array.
- * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
  */
 
-/**
- * Depth-first search
- */
+#ifndef LEETCODE_PALINDROME_PARTITIONING_H
+#define LEETCODE_PALINDROME_PARTITIONING_H
+
 char ***partition_131_1(char *s, int **columnSizes, int *returnSize);
-
-/**
- * Dynamic programming
- */
-char ***partition_131_2(char *s, int **columnSizes, int *returnSize);
 
 #endif //LEETCODE_PALINDROME_PARTITIONING_H
