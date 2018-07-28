f3a77a2ae9e095e9ea337613daf167cd49aa5e1a
Binbin Qian <qianbinbin@hotmail.com>
2018-07-28 17:15:17 +0800

    [Java] 144. Binary Tree Preorder Traversal - approach 2: iteration

--- src/main/java/io/binac/leetcode/BinaryTreePreorderTraversal.java
+++ src/main/java/io/binac/leetcode/BinaryTreePreorderTraversal.java
@@ -4,6 +4,8 @@ import io.binac.leetcode.util.TreeNode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Stack;
 
 /**
  * Given a binary tree, return the preorder traversal of its nodes' values.
@@ -33,6 +35,23 @@ public class BinaryTreePreorderTraversal {
         public List<Integer> preorderTraversal(TreeNode root) {
             List<Integer> result = new ArrayList<>();
             preOrderTraversal(root, result);
+            return result;
+        }
+    }
+
+    public static class Solution2 {
+        public List<Integer> preorderTraversal(TreeNode root) {
+            List<Integer> result = new ArrayList<>();
+            if (root == null) return result;
+
+            Stack<TreeNode> stack = new Stack<>();
+            stack.push(root);
+            while (!stack.empty()) {
+                TreeNode node = Objects.requireNonNull(stack.pop());
+                result.add(node.val);
+                if (node.right != null) stack.push(node.right);
+                if (node.left != null) stack.push(node.left);
+            }
             return result;
         }
     }
--- src/test/java/io/binac/leetcode/BinaryTreePreorderTraversalTest.java
+++ src/test/java/io/binac/leetcode/BinaryTreePreorderTraversalTest.java
@@ -13,6 +13,8 @@ class BinaryTreePreorderTraversalTest {
 
     private final BinaryTreePreorderTraversal.Solution1 solution1 = new BinaryTreePreorderTraversal.Solution1();
 
+    private final BinaryTreePreorderTraversal.Solution2 solution2 = new BinaryTreePreorderTraversal.Solution2();
+
     private final TreeNode root = Trees.asTree(1, null, 2, 3);
 
     private final List<Integer> expected = Arrays.asList(1, 2, 3);
@@ -20,5 +22,10 @@ class BinaryTreePreorderTraversalTest {
     @Test
     void test1() {
         assertEquals(expected, solution1.preorderTraversal(root));
+    }
+
+    @Test
+    void test2() {
+        assertEquals(expected, solution2.preorderTraversal(root));
     }
 }
