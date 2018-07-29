0fde099bc575bb6a2f79f5d09b8286ea4835e3cf
Binbin Qian <qianbinbin@hotmail.com>
2018-07-29 15:20:23 +0800

    [C] Refactor 147. Insertion Sort List

--- src/main/c/insertion_sort_list.c
+++ src/main/c/insertion_sort_list.c
@@ -1,23 +1,25 @@
-#include <insertion_sort_list.h>
+#include "insertion_sort_list.h"
+
 #include <stdlib.h>
 
-struct ListNode *insertionSortList_147(struct ListNode *head) {
+struct ListNode *insertionSortList_147_1(struct ListNode *head) {
+    if (head == NULL) return NULL;
+
     struct ListNode *dummy = (struct ListNode *) malloc(sizeof(struct ListNode));
     dummy->next = head;
-    struct ListNode *tail = dummy, *pre;
-    struct ListNode *cur = tail->next;
-    while (cur != NULL) {
+    struct ListNode *p = head->next, *pre = dummy, *unsorted;
+    head->next = NULL;
+
+    while (p != NULL) {
+        while (pre->next != NULL && p->val > pre->next->val)
+            pre = pre->next;
+        unsorted = p->next;
+        p->next = pre->next;
+        pre->next = p;
+        p = unsorted;
         pre = dummy;
-        while (pre != tail && pre->next->val < cur->val) pre = pre->next;
-        if (pre != tail) {
-            tail->next = cur->next;
-            cur->next = pre->next;
-            pre->next = cur;
-        } else {
-            tail = cur;
-        }
-        cur = tail->next;
     }
+
     head = dummy->next;
     free(dummy);
     return head;
--- src/test/c/insertion_sort_list_test.cpp
+++ src/test/c/insertion_sort_list_test.cpp
@@ -1,14 +1,25 @@
 #include <gtest/gtest.h>
 
 extern "C" {
-#include <insertion_sort_list.h>
+#include "insertion_sort_list.h"
 }
 
-TEST(leetcode_147, normal) {
-    int nums[] = {7, 5, 1, 3, 5, 9, 1, 2};
-    struct ListNode *list = list_create(nums, 8);
-    list_print(list);
-    list = insertionSortList_147(list);
-    list_print(list);
-    list_free(list);
+#define ARR_SIZE(a) (sizeof(a) / sizeof((a)[0]))
+
+int list1[] = {4, 2, 1, 3};
+
+int list2[] = {-1, 5, 3, 4, 0};
+
+TEST(insertion_sort_list_test, insertionSortList_147_1) {
+    struct ListNode *head1 = list_create(list1, ARR_SIZE(list1));
+    list_print(head1);
+    head1 = insertionSortList_147_1(head1);
+    list_print(head1);
+    list_free(head1);
+
+    struct ListNode *head2 = list_create(list2, ARR_SIZE(list2));
+    list_print(head2);
+    head2 = insertionSortList_147_1(head2);
+    list_print(head2);
+    list_free(head2);
 }
