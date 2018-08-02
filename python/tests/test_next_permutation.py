048dfa3caacf509e6911157c7cd34f170381c758
Binbin Qian <qianbinbin@hotmail.com>
2018-08-02 15:11:14 +0800

    [Python] 31. Next Permutation

---
+++ python/tests/test_next_permutation.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+
+from next_permutation import *
+
+solution1 = Solution1()
+
+expected1 = [1, 3, 2]
+
+expected2 = [1, 2, 3]
+
+expected3 = [1, 5, 1]
+
+
+class TestNextPermutation(TestCase):
+    def test1(self):
+        nums1 = [1, 2, 3]
+        solution1.nextPermutation(nums1)
+        self.assertListEqual(expected1, nums1)
+
+        nums2 = [3, 2, 1]
+        solution1.nextPermutation(nums2)
+        self.assertListEqual(expected2, nums2)
+
+        nums3 = [1, 1, 5]
+        solution1.nextPermutation(nums3)
+        self.assertListEqual(expected3, nums3)
