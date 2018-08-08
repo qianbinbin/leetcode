360713cee038c2e2675252157ce8e57182f72c35
Binbin Qian <qianbinbin@hotmail.com>
2018-08-08 15:00:55 +0800

    [Python] 75. Sort Colors

---
+++ python/tests/test_sort_colors.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+
+from sort_colors import *
+
+solution1 = Solution1()
+
+expected = [0, 0, 1, 1, 2, 2]
+
+
+class TestSortColors(TestCase):
+    def test1(self):
+        nums = [2, 0, 2, 1, 1, 0]
+        solution1.sortColors(nums)
+        self.assertListEqual(expected, nums)
