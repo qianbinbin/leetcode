59b37ae81c9bde9177f7d510150ffeffb0da2e4c
Binbin Qian <qianbinbin@hotmail.com>
2018-08-10 22:55:45 +0800

    [Python] 28. Implement strStr() - approach 1: brute force

---
+++ python/leetcodepy/implement_strstr.py
@@ -0,0 +1,47 @@
+"""
+Implement strStr().
+
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+
+Input: haystack = "hello", needle = "ll"
+Output: 2
+
+Example 2:
+
+Input: haystack = "aaaaa", needle = "bba"
+Output: -1
+
+Clarification:
+
+What should we return when needle is an empty string? This is a great question to ask during an interview.
+
+For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+"""
+
+
+class Solution1:
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        size1, size2 = len(haystack), len(needle)
+        if size1 < size2:
+            return -1
+        if size2 == 0:
+            return 0
+        i, j = 0, 0
+        while i <= size1 - size2:
+            j = 0
+            while j < size2:
+                if haystack[i + j] != needle[j]:
+                    i += 1
+                    break
+                else:
+                    j += 1
+            if j == size2:
+                return i
+        return -1
---
+++ python/tests/test_implement_strstr.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+
+from leetcodepy.implement_strstr import *
+
+solution1 = Solution1()
+
+haystack1 = "hello"
+
+needle1 = "ll"
+
+expected1 = 2
+
+haystack2 = "aaaaa"
+
+needle2 = "bba"
+
+expected2 = -1
+
+
+class TestImplementStrStr(TestCase):
+    def test1(self):
+        self.assertEqual(expected1, solution1.strStr(haystack1, needle1))
+        self.assertEqual(expected2, solution1.strStr(haystack2, needle2))
