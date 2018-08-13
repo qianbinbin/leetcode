8e1c54fb701f5852c8b393f554435b137a786d46
Binbin Qian <qianbinbin@hotmail.com>
2018-08-13 16:24:41 +0800

    [Python] 101. Symmetric Tree - approach 2: iteration

--- python/leetcodepy/symmetric_tree.py
+++ python/leetcodepy/symmetric_tree.py
@@ -20,6 +20,7 @@ But the following [1,2,2,null,3,null,3] is not:
 Note:
 Bonus points if you could solve it both recursively and iteratively.
 """
+from queue import Queue
 from typing import Optional
 
 from .utils import TreeNode
@@ -45,3 +46,29 @@ class Solution1:
         if root1.val != root2.val:
             return False
         return self.is_symmetric(root1.left, root2.right) and self.is_symmetric(root1.right, root2.left)
+
+
+class Solution2:
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if root is None:
+            return True
+        q = Queue()
+        q.put(root.left)
+        q.put(root.right)
+        while not q.empty():
+            node1, node2 = q.get(), q.get()
+            if node1 is None and node2 is None:
+                continue
+            if node1 is None or node2 is None:
+                return False
+            if node1.val != node2.val:
+                return False
+            q.put(node1.left)
+            q.put(node2.right)
+            q.put(node1.right)
+            q.put(node2.left)
+        return True
--- python/tests/test_symmetric_tree.py
+++ python/tests/test_symmetric_tree.py
@@ -5,6 +5,8 @@ from leetcodepy.utils import trees
 
 solution1 = Solution1()
 
+solution2 = Solution2()
+
 root1 = trees.from_values(1, 2, 2, 3, 4, 4, 3)
 
 root2 = trees.from_values(1, 2, 2, None, 3, None, 3)
@@ -14,3 +16,7 @@ class TestSymmetricTree(TestCase):
     def test1(self):
         self.assertTrue(solution1.isSymmetric(root1))
         self.assertFalse(solution1.isSymmetric(root2))
+
+    def test2(self):
+        self.assertTrue(solution2.isSymmetric(root1))
+        self.assertFalse(solution2.isSymmetric(root2))
