0eb1e6add8a89f92ab4a141b7036886c37f138c1
Binbin Qian <qianbinbin@hotmail.com>
2018-08-14 17:04:25 +0800

    [Python] 116. Populating Next Right Pointers in Each Node - approach 1: recursion

---
+++ python/tests/test_populating_next_right_pointers_in_each_node.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+from leetcodepy.populating_next_right_pointers_in_each_node import *
+
+solution1 = Solution1()
+
+
+class TestPopulatingNextRightPointersInEachNode(TestCase):
+    def test1(self):
+        # TODO
+        pass
