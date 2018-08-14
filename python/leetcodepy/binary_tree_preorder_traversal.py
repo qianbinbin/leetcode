748f57d01805b8ca0ab6bc4756213d6efeb57a43
Binbin Qian <qianbinbin@hotmail.com>
2018-08-14 21:21:11 +0800

    [Python] 144. Binary Tree Preorder Traversal - approach 2: iteration

--- python/leetcodepy/binary_tree_preorder_traversal.py
+++ python/leetcodepy/binary_tree_preorder_traversal.py
@@ -35,3 +35,23 @@ class Solution1:
         result.append(root.val)
         self.pre_order(root.left, result)
         self.pre_order(root.right, result)
+
+
+class Solution2:
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        result = []
+        if root is None:
+            return result
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            result.append(node.val)
+            if node.right is not None:
+                stack.append(node.right)
+            if node.left is not None:
+                stack.append(node.left)
+        return result
--- python/tests/test_binary_tree_preorder_traversal.py
+++ python/tests/test_binary_tree_preorder_traversal.py
@@ -5,6 +5,8 @@ from leetcodepy.utils import trees
 
 solution1 = Solution1()
 
+solution2 = Solution2()
+
 root = trees.from_values(1, None, 2, 3)
 
 expected = [1, 2, 3]
@@ -13,3 +15,6 @@ expected = [1, 2, 3]
 class TestBinaryTreePreorderTraversal(TestCase):
     def test1(self):
         self.assertListEqual(expected, solution1.preorderTraversal(root))
+
+    def test2(self):
+        self.assertListEqual(expected, solution2.preorderTraversal(root))
