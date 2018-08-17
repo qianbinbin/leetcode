1cf91a37d269a0109ff65dc2d73a0318e6899bb0
Binbin Qian <qianbinbin@hotmail.com>
2018-08-17 22:18:36 +0800

    [Python] Point

--- python/leetcodepy/utils/__init__.py
+++ python/leetcodepy/utils/__init__.py
@@ -3,3 +3,9 @@ from .linked_lists import RandomListNode
 from .trees import TreeNode
 from .trees import TreeLinkNode
 from .graphs import UndirectedGraphNode
+
+
+class Point:
+    def __init__(self, a=0, b=0):
+        self.x = a
+        self.y = b
