ee7283a941f46ac332d57a30124ef524f5cade95
Binbin Qian <qianbinbin@hotmail.com>
2018-08-17 23:13:45 +0800

    [Python] 128. Longest Consecutive Sequence - approach 2: union-find

--- python/leetcodepy/longest_consecutive_sequence.py
+++ python/leetcodepy/longest_consecutive_sequence.py
@@ -27,3 +27,57 @@ class Solution1:
                     end += 1
                 result = max(result, end - num)
         return result
+
+
+class Solution2:
+    class UnionFind:
+        def __init__(self, n):
+            if n <= 0:
+                raise ValueError
+            self.__parent = [i for i in range(n)]
+            self.__rank = [0] * n
+
+        def find(self, x):
+            if x == self.__parent[x]:
+                return x
+            self.__parent[x] = self.find(self.__parent[x])
+            return self.__parent[x]
+
+        def union(self, x1, x2):
+            root1, root2 = self.find(x1), self.find(x2)
+            if self.__rank[root1] < self.__rank[root2]:
+                self.__parent[root1] = self.__parent[root2]
+            else:
+                self.__parent[root2] = self.__parent[root1]
+                if self.__rank[root1] == self.__rank[root2]:
+                    self.__rank[root2] += 1
+
+        def max_size(self):
+            result = 0
+            n = len(self.__parent)
+            size = [0] * n
+            for i in range(n):
+                root = self.find(i)
+                size[root] += 1
+                result = max(result, size[root])
+            return result
+
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        if n == 0:
+            return 0
+        dict_ = {}
+        uf = self.UnionFind(n)
+        for i, num in enumerate(nums):
+            if num in dict_:
+                continue
+            dict_[num] = i
+            if num - 1 in dict_:
+                uf.union(dict_[num - 1], i)
+            if num + 1 in dict_:
+                uf.union(dict_[num + 1], i)
+        return uf.max_size()
--- python/tests/test_longest_consecutive_sequence.py
+++ python/tests/test_longest_consecutive_sequence.py
@@ -4,6 +4,8 @@ from leetcodepy.longest_consecutive_sequence import *
 
 solution1 = Solution1()
 
+solution2 = Solution2()
+
 nums = [100, 4, 200, 1, 3, 2]
 
 expected = 4
@@ -12,3 +14,6 @@ expected = 4
 class TestLongestConsecutiveSequence(TestCase):
     def test1(self):
         self.assertEqual(expected, solution1.longestConsecutive(nums))
+
+    def test2(self):
+        self.assertEqual(expected, solution2.longestConsecutive(nums))
