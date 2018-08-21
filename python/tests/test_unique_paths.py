602ddc5a813b093acf39247c4c6748eebc762a51
Binbin Qian <qianbinbin@hotmail.com>
2018-08-22 02:00:17 +0800

    [Python] 62. Unique Paths - approach 1: dynamic programming

---
+++ python/tests/test_unique_paths.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+
+from leetcodepy.unique_paths import *
+
+solution1 = Solution1()
+
+m1 = 3
+
+n1 = 2
+
+expected1 = 3
+
+m2 = 7
+
+n2 = 3
+
+expected2 = 28
+
+
+class TestUniquePaths(TestCase):
+    def test1(self):
+        self.assertEqual(expected1, solution1.uniquePaths(m1, n1))
+        self.assertEqual(expected2, solution1.uniquePaths(m2, n2))
