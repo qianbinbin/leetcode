dd984e0a0c5d907558c07952afb4a851635fbcfd
Binbin Qian <qianbinbin@hotmail.com>
2018-08-22 23:24:51 +0800

    [Python] 132. Palindrome Partitioning II - approach 2

--- python/leetcodepy/palindrome_partitioning_ii.py
+++ python/leetcodepy/palindrome_partitioning_ii.py
@@ -26,3 +26,23 @@ class Solution1:
                     dp[i][j] = True
                     cut[i] = min(cut[i], cut[j + 1] + 1)
         return cut[0]
+
+
+class Solution2:
+    def minCut(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        size = len(s)
+        cut = [i - 1 for i in range(size + 1)]
+        for i in range(size):
+            len_ = 0
+            while i - len_ >= 0 and i + len_ < size and s[i - len_] == s[i + len_]:
+                cut[i + len_ + 1] = min(cut[i + len_ + 1], cut[i - len_] + 1)
+                len_ += 1
+            len_ = 0
+            while i - len_ >= 0 and i + len_ + 1 < size and s[i - len_] == s[i + len_ + 1]:
+                cut[i + len_ + 2] = min(cut[i + len_ + 2], cut[i - len_] + 1)
+                len_ += 1
+        return cut[size]
--- python/tests/test_palindrome_partitioning_ii.py
+++ python/tests/test_palindrome_partitioning_ii.py
@@ -4,6 +4,8 @@ from leetcodepy.palindrome_partitioning_ii import *
 
 solution1 = Solution1()
 
+solution2 = Solution2()
+
 s = "aab"
 
 expected = 1
@@ -12,3 +14,6 @@ expected = 1
 class TestPalindromePartitioningII(TestCase):
     def test1(self):
         self.assertEqual(expected, solution1.minCut(s))
+
+    def test2(self):
+        self.assertEqual(expected, solution2.minCut(s))
