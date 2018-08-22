ad4162ae64d8b25ced5768f07536816ffe6ff2cd
Binbin Qian <qianbinbin@hotmail.com>
2018-08-22 21:44:05 +0800

    [Python] 123. Best Time to Buy and Sell Stock III - approach 1

---
+++ python/leetcodepy/best_time_to_buy_and_sell_stock_iii.py
@@ -0,0 +1,45 @@
+"""
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete at most two transactions.
+
+Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+
+Example 1:
+
+Input: [3,3,5,0,0,3,1,4]
+Output: 6
+Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+             Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
+
+Example 2:
+
+Input: [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+             Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are
+             engaging multiple transactions at the same time. You must sell before buying again.
+
+Example 3:
+
+Input: [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e. max profit = 0.
+"""
+from typing import List
+
+
+class Solution1:
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        hold1, release1 = float('-inf'), 0
+        hold2, release2 = float('-inf'), 0
+        for price in prices:
+            release2 = max(release2, hold2 + price)
+            hold2 = max(hold2, release1 - price)
+            release1 = max(release1, hold1 + price)
+            hold1 = max(hold1, -price)
+        return release2
---
+++ python/tests/test_best_time_to_buy_and_sell_stock_iii.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+
+from leetcodepy.best_time_to_buy_and_sell_stock_iii import *
+
+solution1 = Solution1()
+
+prices1 = [3, 3, 5, 0, 0, 3, 1, 4]
+
+expected1 = 6
+
+prices2 = [1, 2, 3, 4, 5]
+
+expected2 = 4
+
+prices3 = [7, 6, 4, 3, 1]
+
+expected3 = 0
+
+
+class TestBestTimeToBuyAndSellStockIII(TestCase):
+    def test1(self):
+        self.assertEqual(expected1, solution1.maxProfit(prices1))
+        self.assertEqual(expected2, solution1.maxProfit(prices2))
+        self.assertEqual(expected3, solution1.maxProfit(prices3))
