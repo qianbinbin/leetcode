a4a30686144c7d58b0f70b0d047e9c262d2c8bc8
Binbin Qian <qianbinbin@hotmail.com>
2018-08-24 04:48:26 +0800

    [Python] Interval

--- python/leetcodepy/utils/__init__.py
+++ python/leetcodepy/utils/__init__.py
@@ -9,3 +9,16 @@ class Point:
     def __init__(self, a=0, b=0):
         self.x = a
         self.y = b
+
+
+class Interval:
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
+
+    def __eq__(self, other):
+        if self is other:
+            return True
+        if not isinstance(other, Interval):
+            return False
+        return self.start == other.start and self.end == other.end
