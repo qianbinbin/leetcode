525798ffb247374c4fe816e63f65ac2fa72bc7b9
Binbin Qian <qianbinbin@hotmail.com>
2019-02-22 19:29:45 +0800

    [CPP] 143. Reorder List

---
+++ cpp/src/ReorderList.cpp
@@ -0,0 +1,29 @@
+#include "ReorderList.h"
+
+using namespace lcpp;
+
+void Solution143_1::reorderList(ListNode *head) {
+  if (head == nullptr)
+    return;
+  ListNode *Slow = head, *Fast = head;
+  while (Fast != nullptr && Fast->next != nullptr) {
+    Slow = Slow->next;
+    Fast = Fast->next->next;
+  }
+  ListNode Dummy(0);
+  Dummy.next = Slow;
+  for (auto P = Slow->next; P != nullptr; P = Slow->next) {
+    Slow->next = P->next;
+    P->next = Dummy.next;
+    Dummy.next = P;
+  }
+  ListNode *Tail = &Dummy, *P1 = head, *P2 = Dummy.next;
+  while (P1 != Slow) {
+    Tail->next = P1;
+    Tail = P1;
+    P1 = P1->next;
+    Tail->next = P2;
+    Tail = P2;
+    P2 = P2->next;
+  }
+}
