392fa15fb6d7bb116eceeb68f382c61f59d2dce0
Binbin Qian <qianbinbin@hotmail.com>
2019-03-31 22:44:46 +0800

    [CPP] 134. Gas Station

---
+++ cpp/src/GasStation.cpp
@@ -0,0 +1,19 @@
+#include "GasStation.h"
+
+using namespace lcpp;
+
+int Solution134_1::canCompleteCircuit(std::vector<int> &gas,
+                                      std::vector<int> &cost) {
+  std::vector<int>::size_type I, E = gas.size(), Start = 0;
+  assert(E == cost.size());
+  int Tank = 0, Total = 0;
+  for (I = 0; I != E; ++I) {
+    Tank += gas[I] - cost[I];
+    Total += gas[I] - cost[I];
+    if (Tank < 0) {
+      Start = I + 1;
+      Tank = 0;
+    }
+  }
+  return Total >= 0 ? Start : -1;
+}
---
+++ cpp/unittests/GasStationTest.cpp
@@ -0,0 +1,15 @@
+#include "GasStation.h"
+#include "gtest/gtest.h"
+#include <string>
+
+using namespace lcpp;
+
+TEST(GasStation, Solution134_1) {
+  std::vector<int> Gas1{1, 2, 3, 4, 5}, Cost1{3, 4, 5, 1, 2};
+  const int Expected1 = 3;
+  EXPECT_EQ(Expected1, Solution134_1().canCompleteCircuit(Gas1, Cost1));
+
+  std::vector<int> Gas2{2, 3, 4}, Cost2{3, 4, 3};
+  const int Expected2 = -1;
+  EXPECT_EQ(Expected2, Solution134_1().canCompleteCircuit(Gas2, Cost2));
+}
