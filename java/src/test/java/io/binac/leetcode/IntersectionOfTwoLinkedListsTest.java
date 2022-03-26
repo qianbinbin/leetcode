f3dae25c7887cb4c1ac91ccb5d93206b128fd569
Binbin Qian <qianbinbin@hotmail.com>
2022-03-27 00:37:22 +0800

    160. Intersection of Two Linked Lists

---
+++ java/src/test/java/io/binac/leetcode/IntersectionOfTwoLinkedListsTest.java
@@ -0,0 +1,14 @@
+package io.binac.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IntersectionOfTwoLinkedListsTest {
+    private static final IntersectionOfTwoLinkedLists.Solution1 SOLUTION1 = new IntersectionOfTwoLinkedLists.Solution1();
+
+    @Test
+    void test1() {
+        // TODO
+    }
+}
