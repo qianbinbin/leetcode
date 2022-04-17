de7d6bdd0c4aec9f0e2cdd10ce5c63c594371a1a
Binbin Qian <qianbinbin@hotmail.com>
2022-04-18 01:17:24 +0800

    237. Delete Node in a Linked List

---
+++ java/src/test/java/io/binac/leetcode/DeleteNodeInALinkedListTest.java
@@ -0,0 +1,14 @@
+package io.binac.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeleteNodeInALinkedListTest {
+    private static final DeleteNodeInALinkedList.Solution1 SOLUTION1 = new DeleteNodeInALinkedList.Solution1();
+
+    @Test
+    void test1() {
+        // TODO
+    }
+}
